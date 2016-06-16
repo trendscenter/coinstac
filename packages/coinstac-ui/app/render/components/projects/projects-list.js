0cfd05b333fd0c9ccddd059ab2e18c5e80b1a0b2
Cory Reed <creed@mrn.org>
2016-06-15 22:34:05 -0700

    Clean up projects list component.

--- packages/coinstac-ui/app/render/components/projects/projects-list.js
+++ packages/coinstac-ui/app/render/components/projects/projects-list.js
@@ -1,22 +1,33 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
 import { Button } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import { connect } from 'react-redux';
-import { fetch as fetchProjects, remove as removeProject, setProjects } from 'app/render/state/ducks/projects.js';
+import {
+  fetch as fetchProjects,
+  remove as removeProject,
+  setProjects,
+} from 'app/render/state/ducks/projects.js';
 import { ProjectCard } from './project-card.js';
 import { hilarious } from 'app/render/utils/hilarious-loading-messages.js';
 import app from 'ampersand-app';
-import _ from 'lodash';
 
-class ProjectsList extends React.Component {
-
+class ProjectsList extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
-    const { dispatch } = props;
+    this.state = {
+      ready: false,
+    };
+  }
+
+  componentWillMount() {
+    const { dispatch } = this.props;
+
     dispatch(fetchProjects((err) => {
-      if (err) { return; }
-      this.setState({ ready: true });
+      if (err) {
+        app.notify('error', err.message);
+      } else {
+        this.setState({ ready: true });
+      }
     }));
   }
 
@@ -31,39 +42,45 @@ class ProjectsList extends React.Component {
   }
 
   render() {
-    const { projects, loading } = this.props;
+    const { projects } = this.props;
+
     if (!this.state.ready) {
       return (<span>{hilarious.random()}</span>);
     }
+
     return (
-            <div>
-                <LinkContainer to="/projects/new" id="add_project">
-                    <Button bsStyle="primary" className="pull-right">
-                        <strong>+</strong>
-                        Add Project
-                    </Button>
-                </LinkContainer>
-                <div className="projects-list">
-                    {projects.map(project => {
-                      return (
-                            <ProjectCard
-                              key={'project-card-' + project._id}
-                              {...this.props}
-                              project={project}
-                              delete={evt => this.delete(project)}
-                            />
-                        );
-                    })}
-                </div>
-            </div>
-        );
+      <div>
+        <LinkContainer to="/projects/new" id="add_project">
+          <Button bsStyle="primary" className="pull-right">
+            <strong>+</strong>
+            Add Project
+          </Button>
+        </LinkContainer>
+        <div className="projects-list">
+          {projects.map(project => {
+            return (
+              <ProjectCard
+                key={`project-card-${project._id}`}
+                {...this.props}
+                project={project}
+                delete={() => this.delete(project)}
+              />
+            );
+          })}
+        </div>
+      </div>
+    );
   }
 }
+
+ProjectsList.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  projects: PropTypes.array,
+};
 
 function select(state) {
   return {
     projects: state.projects,
-    loading: state.loading,
   };
 }
 
