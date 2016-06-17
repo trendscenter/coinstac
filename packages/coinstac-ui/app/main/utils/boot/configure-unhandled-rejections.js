508d1a74a4325b40a0b1f73e97283a8d5243e6a4
Cory Reed <creed@mrn.org>
2016-06-17 15:52:05 -0700

    Prevent UI's main process from :fire: blowing up on unhandled rejections.
    Just until electron/electron#6113 is closed.

--- packages/coinstac-ui/app/main/utils/boot/configure-unhandled-rejections.js
+++ packages/coinstac-ui/app/main/utils/boot/configure-unhandled-rejections.js
@@ -1,5 +1,18 @@
 'use strict';
 
-const logUnhandledError = require('app/common/utils/log-unhandled-error.js');
+const app = require('ampersand-app');
 
-process.on('unhandledRejection', logUnhandledError());
+/**
+ * **Handle** unhandled rejections.
+ *
+ * @todo This prevents program shutdown on unhandled rejections, which equate to
+ * unknown program state. Drop this and use
+ * app/common/utils/log-unhandled-error.js once
+ * {@link https://github.com/electron/electron/issues/6113} is closed.
+ *
+ * {@link https://nodejs.org/api/process.html#process_event_unhandledrejection}
+ */
+
+process.on('unhandledRejection', (reason, p) => {
+  app.logger.error('Unhandled rejection at: Promise ', p, ' reason: ', r);
+});
