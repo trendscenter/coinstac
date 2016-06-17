92046e8b18f53075fdb5cfb141db57bd840e7985
Cory Reed <creed@mrn.org>
2016-06-17 16:01:45 -0700

    Refactor project creation components.

--- packages/coinstac-ui/app/render/components/projects/form-add-project.js
+++ packages/coinstac-ui/app/render/components/projects/form-add-project.js
@@ -1,12 +1,12 @@
 import {
   Button,
-  ButtonToolbar,
   ControlLabel,
   FormControl,
   FormGroup,
   HelpBlock,
 } from 'react-bootstrap';
 import { findDOMNode } from 'react-dom';
+import ProjectFiles from './project-files';
 import React, { Component, PropTypes } from 'react';
 
 export default class FormAddProject extends Component {
@@ -14,7 +14,8 @@ export default class FormAddProject extends Component {
     event.preventDefault();
 
     this.props.onSubmit({
-      name: findDOMNode('name').value,
+      files: this.props.files,
+      name: findDOMNode(this.refs.name).value,
     });
   }
 
@@ -22,8 +23,11 @@ export default class FormAddProject extends Component {
     const {
       errors,
       errors: { name: nameError },
+      files,
+      onAddFilesClick,
       onCancel,
       onNameChange,
+      onRemoveFileClick,
     } = this.props;
     const validationState = {
       validationState: nameError ? 'error' : undefined,
@@ -50,13 +54,22 @@ export default class FormAddProject extends Component {
             />
             {helpBlock}
           </FormGroup>
-          <ButtonToolbar className="pull-right">
+
+          <Button
+            onClick={onAddFilesClick}
+            type="button">
+            Add Files
+          </Button>
+          <ProjectFiles files={files} onRemoveFileClick={onRemoveFileClick} />
+
+          <div className="text-right">
             <Button
               bsStyle="link"
               onClick={onCancel}
               type="reset"
             >
               <span className="glyphicon glyphicon-remove" aria-hidden="true"></span>
+              {' '}
               Cancel
             </Button>
             <Button
@@ -65,9 +78,10 @@ export default class FormAddProject extends Component {
               disabled={!!Object.keys(errors).length}
             >
               <span className="glyphicon glyphicon-ok" aria-hidden="true"></span>
+              {' '}
               Add
             </Button>
-          </ButtonToolbar>
+          </div>
         </form>
       </div>
     );
@@ -76,7 +90,10 @@ export default class FormAddProject extends Component {
 
 FormAddProject.propTypes = {
   errors: PropTypes.object.isRequired,
+  files: PropTypes.array.isRequired,
+  onAddFilesClick: PropTypes.func.isRequired,
   onCancel: PropTypes.func.isRequired,
   onNameChange: PropTypes.func.isRequired,
   onSubmit: PropTypes.func.isRequired,
+  onRemoveFileClick: PropTypes.func.isRequired,
 };
