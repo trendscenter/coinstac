6c482f4f5ffe152e69c164cacd12f891ee394431
Cory Reed <creed@mrn.org>
2016-06-27 11:06:27 -0700

    Re-work server logger.
    This adds a log file to /var/log/coinstac/application.log and makes the
    console transport's level configurable via CLI.

--- packages/coinstac-server-core/src/services/logger.js
+++ packages/coinstac-server-core/src/services/logger.js
@@ -1,17 +1,53 @@
 'use strict';
 
+const mkdirp = require('mkdirp-promise');
+const path = require('path');
 const winston = require('winston');
-const level = process.env.COINSTAC_SERVER_LOG_LEVEL || 'info';
 
 /**
  * @module service/logger
+ *
+ * {@link https://www.npmjs.com/package/winston}
  */
 
 /**
- * {@link https://www.npmjs.com/package/winston}
+ * Log file directory.
+ *
+ * @const {string}
+ */
+const LOG_DIR = '/var/log/coinstac';
+
+/**
+ * Log file name.
+ *
+ * @const {string}
+ */
+const LOG_BASE = 'application.log';
+
+/**
+ * Logger instance.
+ *
+ * @type {winston.Logger}
  */
 const logger = new (winston.Logger)({
-  transports: [new (winston.transports.Console)({ level })],
+  transports: [
+    new (winston.transports.Console)({
+      colorize: true,
+      level: 'info',
+    }),
+  ],
 });
+
+/**
+ * Set up file transport async. If an error occurs the executable will catch the
+ * unhandled rejection and shut the server down.
+ */
+mkdirp(LOG_DIR)
+  .then(() => {
+    logger.add(winston.transports.File, {
+      filename: path.join(LOG_DIR, LOG_BASE),
+      level: 'silly',
+    });
+  });
 
 module.exports = logger;
