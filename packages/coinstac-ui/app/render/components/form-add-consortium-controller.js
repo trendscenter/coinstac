trendscenter/coinstac

commit 8549907f02f7b50f03095e05f31c660b8e8a402a

Clean up consortium creation form.
This ensures components pass linting and don't emit missing React proptype errors.
It also adjusts the page/form design to match the project creation form.

packages/coinstac-ui/app/render/components/form-add-consortium-controller.js
@@ -1,13 +1,18 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import FormAddConsortium from './form-add-consortium';
-import { saveConsortium } from 'app/render/state/ducks/consortium';
+import { saveConsortium } from '../state/ducks/consortium';
 import { connect } from 'react-redux';
 
-class FormAddConsortiumController extends React.Component {
+class FormAddConsortiumController extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleClickCancel = this.handleClickCancel.bind(this);
+    this.submit = this.submit.bind(this);
+  }
 
   handleClickCancel() {
-    // hashHistory.push('/consortia');
-    // @TODO why is onClick called immediately on cancel button
+    this.context.router.push('/consortia');
   }
 
   submit(consortium) {
@@ -25,13 +30,18 @@ class FormAddConsortiumController extends React.Component {
 
   render() {
     const { loading } = this.props;
+
     return (
-      <FormAddConsortium
-        ref="add"
-        loading={loading}
-        onCancel={this.handleClickCancel.bind(this)}
-        onSubmit={this.submit.bind(this)}
-      />
+      <div>
+        <div className="page-header clearfix">
+          <h1 className="pull-left">Add New Consortium</h1>
+        </div>
+        <FormAddConsortium
+          loading={!!loading.isLoading}
+          handleCancel={this.handleClickCancel}
+          handleSubmit={this.submit}
+        />
+      </div>
     );
   }
 }

packages/coinstac-ui/app/render/components/form-add-consortium.js
@@ -1,19 +1,21 @@
 import React, { Component, PropTypes } from 'react';
 import { reduxForm } from 'redux-form';
-import { FormGroup, ControlLabel, FormControl, ButtonToolbar, Button } from 'react-bootstrap';
+import { FormGroup, ControlLabel, FormControl, Button } from 'react-bootstrap';
+
 export const fields = ['label', 'description'];
 
 class FormAddConsortium extends Component { // eslint-disable-line
   render() {
     const {
-      fields: { label, description },
+      fields: { description, label },
       handleSubmit,
-      resetForm,
       loading,
+      handleCancel,
+      resetForm,
     } = this.props;
+
     return (
       <form onSubmit={handleSubmit}>
-        <h1>Add New Consortium</h1>
         <FormGroup controlId="name">
           <ControlLabel>Name</ControlLabel>
           <FormControl type="text" placeholder="Enter Name" {...label} />
@@ -29,12 +31,28 @@ class FormAddConsortium extends Component { // eslint-disable-line
             value={description.value || ''}
           />
         </FormGroup>
-        <ButtonToolbar>
-          <Button bsStyle="primary" type="submit" disabled={loading.isLoading}>
-            Submit
-          </Button>
-          <Button onClick={resetForm && this.props.onCancel()}>Cancel</Button>
-        </ButtonToolbar>
+        <div className="clearfix">
+          <div className="pull-right">
+            <Button
+              bsStyle="success"
+              disabled={loading}
+              type="submit"
+            >
+              <span className="glyphicon glyphicon-ok"></span>
+              {' '}
+              Submit
+            </Button>
+            <Button
+              bsStyle="link"
+              onClick={() => resetForm() && handleCancel()}
+              type="reset"
+            >
+              <span className="glyphicon glyphicon-remove"></span>
+              {' '}
+              Cancel
+            </Button>
+          </div>
+        </div>
       </form>
     );
   }
@@ -42,10 +60,9 @@ class FormAddConsortium extends Component { // eslint-disable-line
 
 FormAddConsortium.propTypes = {
   fields: PropTypes.object.isRequired,
+  handleCancel: PropTypes.func.isRequired,
   handleSubmit: PropTypes.func.isRequired,
-  loading: PropTypes.object,
-  onSubmit: PropTypes.func.isRequired,
-  onCancel: PropTypes.func.isRequired,
+  loading: PropTypes.bool.isRequired,
   resetForm: PropTypes.func.isRequired,
 };
 

packages/coinstac-ui/app/render/components/dashboard-consortia.js
@@ -3,7 +3,7 @@ import { Button } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import ConsortiumCard from './consortium-card';
 import { connect } from 'react-redux';
-import { setConsortia, fetchConsortia } from 'app/render/state/ducks/consortia.js';
+import { fetchConsortia } from '../state/ducks/consortia.js';
 import noop from 'lodash/noop';
 
 class DashboardConsortia extends React.Component {
@@ -36,14 +36,16 @@ class DashboardConsortia extends React.Component {
 
     return (
       <div className="dashboard-consortia">
-        <div className="page-header">
-          <h1>Consortia</h1>
+        <div className="page-header clearfix">
+          <h1 className="pull-left">Consortia</h1>
+          <LinkContainer className="pull-right" to="/consortia/new">
+            <Button bsStyle="primary" className="pull-right">
+              <span aria-hidden="true" className="glphicon glyphicon-plus"></span>
+              {' '}
+              Add Consortium
+            </Button>
+          </LinkContainer>
         </div>
-        <LinkContainer to="/consortia/new" id="add_consortium">
-          <Button bsStyle="primary" className="pull-right">
-            Add Consortium
-          </Button>
-        </LinkContainer>
         <div className="row">
           {content}
         </div>

commit 154f610bbf645c7f664d1636cafa008c06c78622

Fix consortium add form issues.
This addresses issues introduced in 8549907f02f7b50f03095e05f31c660b8e8a402a
that caused redux-form to error and not save the consortium.

packages/coinstac-ui/app/render/components/form-add-consortium-controller.js
@@ -38,8 +38,8 @@ class FormAddConsortiumController extends Component {
         </div>
         <FormAddConsortium
           loading={!!loading.isLoading}
-          handleCancel={this.handleClickCancel}
-          handleSubmit={this.submit}
+          onResetForm={this.handleClickCancel}
+          onSubmit={this.submit}
         />
       </div>
     );

packages/coinstac-ui/app/render/components/form-add-consortium.js
@@ -10,7 +10,7 @@ class FormAddConsortium extends Component { // eslint-disable-line
       fields: { description, label },
       handleSubmit,
       loading,
-      handleCancel,
+      onResetForm,
       resetForm,
     } = this.props;
 
@@ -44,7 +44,7 @@ class FormAddConsortium extends Component { // eslint-disable-line
             </Button>
             <Button
               bsStyle="link"
-              onClick={() => resetForm() && handleCancel()}
+              onClick={() => resetForm() && onResetForm()}
               type="reset"
             >
               <span className="glyphicon glyphicon-remove"></span>
@@ -60,10 +60,10 @@ class FormAddConsortium extends Component { // eslint-disable-line
 
 FormAddConsortium.propTypes = {
   fields: PropTypes.object.isRequired,
-  handleCancel: PropTypes.func.isRequired,
   handleSubmit: PropTypes.func.isRequired,
   loading: PropTypes.bool.isRequired,
   resetForm: PropTypes.func.isRequired,
+  onResetForm: PropTypes.func.isRequired,
 };
 
 export default reduxForm({