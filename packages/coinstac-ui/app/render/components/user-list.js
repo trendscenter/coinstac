3c3f0372dba55e3ca084e9982e8e3522e97a952f
Cory Reed <creed@mrn.org>
2016-08-04 16:44:36 -0700

    Refactor user list to not include heading element.

--- packages/coinstac-ui/app/render/components/user-list.js
+++ packages/coinstac-ui/app/render/components/user-list.js
@@ -1,3 +1,4 @@
+import classNames from 'classnames';
 import React, { PropTypes } from 'react';
 import { OverlayTrigger, Tooltip } from 'react-bootstrap';
 import { remote } from 'electron';
@@ -6,33 +7,35 @@ import path from 'path';
 const APP_PATH = remote.app.getAppPath();
 
 export default function UserList(props) {
-  const { users } = props;
-  return (
-    <div className="user-list">
-      <h5 className="user-list-title">Users:</h5>
-      <ul className="list-inline">
-        {users.map((username, i) => {
-          const tooltip = <Tooltip id={`user-item-${i}`}>{username}</Tooltip>;
-          const src = path.join(
-            APP_PATH,
-            `/app/render/images/avatar-${i % 3 + 1}.jpg`
-          );
+  const { size, users } = props;
+  const className = classNames(['user-list', 'list-inline'], {
+    'user-list-large': size === 'large',
+  });
 
-          // TODO: Use users' actual avatars
-          return (
-            <li className="user-list-item" key={i}>
-              <OverlayTrigger overlay={tooltip} placement="bottom">
-                <img src={src} alt={username} />
-              </OverlayTrigger>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
+  return (
+    <ul className={className}>
+      {users.map((username, i) => {
+        const tooltip = <Tooltip id={`user-item-${i}`}>{username}</Tooltip>;
+        const src = path.join(
+          APP_PATH,
+          `/app/render/images/avatar-${i % 3 + 1}.jpg`
+        );
+
+        // TODO: Use users' actual avatars
+        return (
+          <li className="user-list-item" key={i}>
+            <OverlayTrigger overlay={tooltip} placement="bottom">
+              <img src={src} alt={username} />
+            </OverlayTrigger>
+          </li>
+        );
+      })}
+    </ul>
   );
 }
 
 UserList.propTypes = {
+  size: PropTypes.oneOf(['large', 'regular']),
   users: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
