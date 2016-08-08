d2090fab3f18d71be8b876945c1b7d0b9f3ca92f
Cory Reed <creed@mrn.org>
2016-08-08 13:22:27 -0700

    Fix decentralized-algo linting errors.

--- packages/coinstac-decentralized-algorithm-integration/test/declarations/bisect-converge.js
+++ packages/coinstac-decentralized-algorithm-integration/test/declarations/bisect-converge.js
@@ -1,6 +1,8 @@
 'use strict';
 
+/* eslint-disable no-console */
 console.log(require.resolve('coinstac-example-computation-bisect-converge'));
+/* eslint-enable no-console */
 
 module.exports = {
   users: [
