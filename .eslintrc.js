f689c0e74ac2b42e3a84e338f227400aa3d5209c
Cory Reed <creed@mrn.org>
2016-08-08 17:09:09 -0700

    Add 'electron' to ESLint configuration.

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,14 @@ module.exports = {
     mocha: true,
     es6: true
   },
+
+  /**
+   * {@link https://github.com/benmosher/eslint-plugin-import/issues/275}
+   * {@link https://github.com/benmosher/eslint-plugin-import#importcore-modules}
+   */
+  settings: {
+    'import/core-modules': ['electron'],
+  },
   rules: {
     strict: [0, 'global'], // required for node, configurable for browser, https://github.com/eslint/eslint/issues/2785#issuecomment-113254153
     'arrow-body-style': 0,
