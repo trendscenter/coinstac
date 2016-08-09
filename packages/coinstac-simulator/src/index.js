trendscenter/coinstac

commit 15aafdd126de999c56439cee306f79eac8a17bcc

Ensure test runs kill child processes.
This addresses a bug where test runs spawned/forked children and the children
processes weren't killed on test errors. Explicitely hooking in to the
`process` exit event ensures everything's shut down properly.

packages/coinstac-simulator/src/index.js
@@ -11,8 +11,27 @@ const bootComputeServers = require('./boot-compute-servers');
 const bootClients = require('./boot-clients');
 const bootDBServer = require('./boot-db-server');
 const seedCentralDB = require('./seed-central-db');
+const flatten = require('lodash/flatten');
+// const values = require('lodash/values');
 
-const processes = {};
+/**
+ * Processes.
+ *
+ * Storage for child processes.
+ *
+ * @const {Object}
+ * @property {(ChildProcess|null)} db
+ * @property {(ChildProcess[]|null)} localhost
+ * @property {(ChildProcess|null)} remote
+ */
+const processes = {
+  db: null,
+  local: null,
+  remote: null,
+};
+
+// Ensure all processes are killed
+process.on('exit', module.exports.killProcesses);
 
 module.exports = {
   /**
@@ -77,5 +96,12 @@ module.exports = {
         .catch((err) => reject(err));
       });
     });
+  },
+
+  /**
+   * Send kill signals to all processes.
+   */
+  killProcesses() {
+    flatten(values(processes)).forEach(p => p.kill());
   },
 };

scripts/test.js
@@ -10,8 +10,10 @@
 const async = require('async');
 const exec = require('shelljs').exec;
 
+const children = [];
+
 function getExec(dirPath, callback) {
-  return exec('npm test', {
+  const child = exec('npm test', {
     async: true,
     cwd: dirPath,
     silent: false,
@@ -22,7 +24,17 @@ function getExec(dirPath, callback) {
       callback(null, stdout.toString());
     }
   });
+
+  children.push(child);
+
+  return child;
 }
+
+function killChildren() {
+  children.forEach(c => c.kill());
+}
+
+process.on('exit', killChildren);
 
 async.series([
   cb1 => {
@@ -43,13 +55,14 @@ async.series([
       } else {
         cb1(null, response);
       }
-      children.forEach(c => c.kill());
     });
   },
   cb2 => {
     getExec('./packages/coinstac-decentralized-algorithm-integration', cb2);
   },
 ], (error, results) => {
+  killChildren(); // Ensure child processes are killed
+
   /* eslint-disable no-console */
   if (error) {
     console.error(error);
@@ -58,4 +71,3 @@ async.series([
   }
   /* eslint-enable no-console */
 });
-

commit d68b11c1ed3df9c83cd3d79f077b39e3bd0b550f

Fix decentralized-algo process exiting bug.
This addresses a bug introduced in 15aafdd126de999c56439cee306f79eac8a17bcc
where a non-defined listener was registered on `process`.

packages/coinstac-simulator/src/index.js
@@ -31,7 +31,9 @@ const processes = {
 };
 
 // Ensure all processes are killed
-process.on('exit', module.exports.killProcesses);
+process.on('exit', () => {
+  flatten(values(processes)).forEach(p => p.kill());
+});
 
 module.exports = {
   /**
@@ -96,12 +98,5 @@ module.exports = {
         .catch((err) => reject(err));
       });
     });
-  },
-
-  /**
-   * Send kill signals to all processes.
-   */
-  killProcesses() {
-    flatten(values(processes)).forEach(p => p.kill());
   },
 };