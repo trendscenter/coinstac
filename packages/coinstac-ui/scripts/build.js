f1a080a61c639a74d62682f439aa9221f31aa20f
Ross Kelly <rkelly@mrn.org>
2016-08-12 04:20:57 -0600

    build and rebuild native for current plat+arch

--- packages/coinstac-ui/scripts/build.js
+++ packages/coinstac-ui/scripts/build.js
@@ -1,40 +1,55 @@
-const packager = require('electron-packager');
+const prom = require('bluebird').promisify;
+const packager = prom(require('electron-packager'));
 const zip = require('tar.gz');
 const fs = require('fs');
+const os = require('os');
+const rebuild = require('electron-rebuild');
+const electron = require('electron-prebuilt');
+const childProcess = require('child_process');
+
 const options = {
-  arch: 'all',
   asar: true,
   dir: `${__dirname}/../`,
   icon: `${__dirname}/../app/render/images/logo`,
   name: 'coinstac',
   overwrite: true,
-  platform: 'all',
   prune: true,
 };
-const outputDirs = [
-  'coinstac-darwin-x64',
-  'coinstac-linux-ia32',
-  'coinstac-linux-x64',
-  'coinstac-mas-x64',
-  'coinstac-win32-ia32',
-  'coinstac-win32-x64',
-];
 
-packager(options, (err, appPaths) => {
-  if (err) {
-    console.log(`Build failed with: ${err}`); // eslint-disable-line no-console
-  } else {
-    console.log(`Finished building at: ${appPaths}`); // eslint-disable-line no-console
-    console.log('Now tarring...'); // eslint-disable-line no-console
-
-    outputDirs.forEach((dir) => {
-      const read = zip().createReadStream(`./${dir}`);
-      const write = fs.createWriteStream(`./${dir}.tar.gz`);
-
-      read.pipe(write);
-      write.on('close', () => {
-        console.log(`Finished zipping ${dir}`); // eslint-disable-line no-console
+const buildNative = () => {
+  return rebuild.shouldRebuildNativeModules(electron)
+  .then(should => {
+    if (should) {
+      let electronVersion = childProcess.execSync(`${electron} --version`, {
+        encoding: 'utf8',
       });
-    });
-  }
+      electronVersion = electronVersion.match(/v(\d+\.\d+\.\d+)/)[1];
+
+      return rebuild.installNodeHeaders(electronVersion)
+      .then(() => rebuild.rebuildNativeModules(electronVersion, `${__dirname}/../node_modules`));
+    }
+  });
+};
+// TODO: build mult arch when possible
+buildNative()
+.then(() => {
+  options.arch = os.arch();
+  options.platform = os.platform();
+  return packager(options);
+})
+.then((appPath) => {
+  console.log(`Finished building at: ${appPath}`); // eslint-disable-line no-console
+  console.log('Now tarring...'); // eslint-disable-line no-console
+  const dir = `${__dirname}/../coinstac-${os.platform()}-${os.arch()}`;
+
+  const read = zip().createReadStream(`${dir}`);
+  const write = fs.createWriteStream(`${dir}.tar.gz`);
+
+  read.pipe(write);
+  write.on('close', () => {
+    console.log(`Finished zipping ${dir}`); // eslint-disable-line no-console
+  });
+})
+.catch(err => {
+  console.log(`Build failed with: ${err}`); // eslint-disable-line no-console
 });
