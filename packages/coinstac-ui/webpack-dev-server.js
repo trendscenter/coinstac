5355d0a7ef6c41d97de25226cb3d770cda1674fc
Cory Reed <creed@mrn.org>
2016-08-16 09:52:47 -0600

    Implement Webpack dashboard.

--- packages/coinstac-ui/webpack-dev-server.js
+++ packages/coinstac-ui/webpack-dev-server.js
@@ -1,15 +1,21 @@
 'use strict';
 
+const Dashboard = require('webpack-dashboard');
+const DashboardPlugin = require('webpack-dashboard/plugin');
 const webpack = require('webpack');
 const WebpackDevServer = require('webpack-dev-server');
 
 const config = require('./webpack.config');
+const dashboard = new Dashboard();
 const port = config.port;
+
+config.plugins.push(new DashboardPlugin(dashboard.setData));
 
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
   historyApiFallback: true,
+  quiet: true,
 }).listen(port, 'localhost', err => {
   /* eslint-disable no-console */
   if (err) {
