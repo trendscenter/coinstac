625a51ab2795855bbfb302143d2a45292fc1144b
Cory Reed <creed@mrn.org>
2016-08-25 17:13:33 -0700

    Clean up client booting.

--- packages/coinstac-simulator/src/boot-clients.js
+++ packages/coinstac-simulator/src/boot-clients.js
@@ -1,49 +1,77 @@
 'use strict';
 
-/**
- * @private
- * @module boot-clients
- */
-
 require('./handle-errors');
 
 const cp = require('child_process');
-const chalk = require('chalk');
-const path = require('path');
 const logger = require('./logger');
-const userChatOK = chalk.magenta;
+const path = require('path');
 
-module.exports = function bootClients(declPath) {
+/**
+ * Get ready client.
+ *
+ * @param {string} username
+ * @param {string} declPath
+ * @param {boolean} [verbose=false]
+ * @returns {Promise}
+ */
+function getReadyClient(username, declPath, verbose) {
+  return new Promise((resolve, reject) => {
+    const client = cp.fork(path.resolve(__dirname, './boot-client.js'), {
+      cwd: process.cwd(),
+      silent: true,
+    });
+
+    function messageHandler(message) {
+      if ('ready' in message && message.ready) {
+        client.removeListener('message', messageHandler);
+        resolve(client);
+      } else {
+        reject(new Error('Client sent non-ready message first'));
+      }
+    }
+
+    client.on('exit', code => {
+      if (code) {
+        throw new Error(`Client process exited with code ${code}`);
+      }
+    });
+    client.on('message', messageHandler);
+    client.stderr.on('data', data => {
+      logger.error(`USER ${username} [${client.pid}]: ${data.slice(0, -1)}`);
+    });
+
+    if (verbose) {
+      client.stdout.on('data', data => {
+        logger.info(`USER ${username} [${client.pid}]: ${data.slice(0, -1)}`);
+      });
+    }
+
+    client.send({
+      boot: {
+        declPath,
+        username,
+      },
+    });
+  });
+}
+
+/**
+ * Boot clients.
+ * @module
+ *
+ * @param {string} declPath
+ * @returns {Promise} Resolves with an array of forked client processes
+ */
+function bootClients(declPath) {
   const decl = require(declPath); // eslint-disable-line global-require
   const usernames = typeof decl.users[0] === 'string' ?
     decl.users :
     decl.users.map(user => user.username);
-  return Promise.all(
-    usernames.map((username) => { // eslint-disable-line
-      return new Promise((res, rej) => {
-        const userProcess = cp.fork(
-          path.resolve(__dirname, './boot-client'),
-          { cwd: process.cwd(), silent: true }
-        );
-        userProcess.on('message', (m) => {
-          if (m.ready) { return res(userProcess); }
-          if (m.toredown) { return; } // handled by `run.js`
-          rej(m); // err if we don't receive ready
-        });
-        userProcess.send({ boot: { declPath, username } });
-        userProcess.on('exit', (code) => {
-          if (code) { throw new Error(`user process exited with ${code}`); }
-        });
-        userProcess.stdout.on('data', (data) => {
-          if (!decl.verbose) { return; }
-          const content = data.slice(0, -1);
-          logger.info(userChatOK(`USER ${username} [${userProcess.pid}]: ${content}`));
-        });
-        userProcess.stderr.on('data', (data) => {
-          const content = data.slice(0, -1);
-          logger.error(`USER ${username} [${userProcess.pid}]: ${content}`);
-        });
-      });
-    })
-  );
-};
+
+  return Promise.all(usernames.map(username => {
+    return getReadyClient(username, declPath, decl.verbose);
+  }));
+}
+
+module.exports = bootClients;
+
