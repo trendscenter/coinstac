trendscenter/coinstac

commit cf67e01d7e72b67fd98e7599cd6bd3f2eb172e98

Refactor computation registry's computation-from-source test.
The `ComputationRegistry#_getFromSource` test was hitting the network. Moving
the npm install method to the class makes it stub-able, allowing the test to run
100% locally. This also refactors the method and test to make them more
readable.

packages/coinstac-common/src/services/classes/computation-registry.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const assign = require('lodash/assign');
+const bluebird = require('bluebird');
 const compact = require('lodash/compact');
 const concatStream = require('concat-stream');
 const DecentralizedComputation =
@@ -140,35 +141,6 @@ function getTarballUrl(options) {
   });
 }
 
-/**
- * Run `npm install` on a path.
- * @private
- * @param {string} path
- * @returns {Promise}
- */
-function runNPMInstall(path) {
-  return new Promise((resolve, reject) => {
-    const child = spawn('npm', ['install', '--production'], { cwd: path });
-
-    let error;
-    let out;
-
-    child.stderr.pipe(concatStream(data => {
-      error = data.toString();
-    }));
-    child.stdout.pipe(concatStream(data => {
-      out = data.toString();
-    }));
-
-    child.on('close', exitCode => {
-      /* istanbul ignore if */
-      if (exitCode) {
-        return reject(error);
-      }
-      return resolve(out);
-    });
-  });
-}
 
 /**
  * Computation registry.
@@ -285,50 +257,46 @@ class ComputationRegistry {
    * @returns {Promise}
    */
   _getFromSource(name, version) {
-    const path = this._getComputationPath(name, version);
+    const computationPath = this._getComputationPath(name, version);
 
-    return getTarballUrl({
-      github: this.github,
-      name,
-      registry: this.registry,
-      version,
-    })
-    .then(tarballUrl => {
-      return new Promise((resolve, reject) => {
-        mkdirp(path, (err) => {
-          if (err) {
-            reject(err);
-          } else {
-            const parsedUrl = url.parse(tarballUrl);
-            const tarExtract = tar.extract(path, {
-              map: header => {
-                // Ensure the tarball isn't unpacked into a deep directory
-                // TODO: Add test to make sure this is needed
-                header.name = tail(header.name.split('/')).join('/');
+    return Promise.all([
+      getTarballUrl({
+        github: this.github,
+        name,
+        registry: this.registry,
+        version,
+      }),
+      bluebird.promisify(mkdirp)(computationPath),
+    ])
+      .then(([tarballUrl]) => {
+        const parsedUrl = url.parse(tarballUrl);
+        const tarExtract = tar.extract(computationPath, {
+          map: header => {
+            // Ensure the tarball isn't unpacked into a deep directory
+            // TODO: Add test to make sure this is needed
+            header.name = tail(header.name.split('/')).join('/');
 
-                return header;
-              },
-            });
-
-            followRedirects.https.get({
-              hostname: parsedUrl.hostname,
-              path: parsedUrl.path,
-              headers: {
-                'User-Agent': 'COINSTAC',
-              },
-              protocol: parsedUrl.protocol,
-            }, res => {
-              res.pipe(gunzipMaybe()).pipe(tarExtract);
-            }).on('error', reject);
-
-            tarExtract.on('error', reject);
-            tarExtract.on('finish', resolve);
-          }
+            return header;
+          },
         });
-      });
-    })
-    .then(() => runNPMInstall(path))
-    .then(() => this._getFromDisk(name, version));
+
+        return new Promise((resolve, reject) => {
+          followRedirects.https.get({
+            hostname: parsedUrl.hostname,
+            path: parsedUrl.path,
+            headers: {
+              'User-Agent': 'COINSTAC',
+            },
+            protocol: parsedUrl.protocol,
+          }, res => {
+            res.pipe(gunzipMaybe()).pipe(tarExtract);
+          }).on('error', reject);
+          tarExtract.on('error', reject);
+          tarExtract.on('finish', resolve);
+        });
+      })
+      .then(() => ComputationRegistry.runNPMInstall(path))
+      .then(() => this._getFromDisk(name, version));
   }
 
   /**
@@ -448,6 +416,38 @@ class ComputationRegistry {
       return [matches[1], matches[2]];
     }
     return null;
+  }
+
+  /**
+   * Run `npm install` on a path.
+   * @private
+   * @static
+   *
+   * @param {string} path
+   * @returns {Promise}
+   */
+  static runNPMInstall(path) {
+    return new Promise((resolve, reject) => {
+      const child = spawn('npm', ['install', '--production'], { cwd: path });
+
+      let error;
+      let out;
+
+      child.stderr.pipe(concatStream(data => {
+        error = data.toString();
+      }));
+      child.stdout.pipe(concatStream(data => {
+        out = data.toString();
+      }));
+
+      child.on('close', exitCode => {
+        /* istanbul ignore if */
+        if (exitCode) {
+          return reject(error);
+        }
+        return resolve(out);
+      });
+    });
   }
 }
 

packages/coinstac-common/test/services/computation-registry.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const assign = require('lodash/assign');
+const bluebird = require('bluebird');
 const clone = require('lodash/clone');
 const ComputationRegistry =
     require('../../src/services/classes/computation-registry');
@@ -282,11 +283,14 @@ tape('gets computation from source', t => {
 
   const instance = factory();
   const name = registry[2].name;
+  const readdirAsync = bluebird.promisify(fs.readdir);
   const version = registry[2].tags[0];
 
   const slug = `${name}@${version}`;
   const stubs = setupNetworkStubs(slug);
   const githubStub = stubs.githubStub;
+  const installStub = sinon.stub(ComputationRegistry, 'runNPMInstall')
+    .returns(Promise.resolve());
   const requestStub = stubs.requestStub;
 
     // Expected tarball URL, taken from the mock JSON
@@ -294,78 +298,60 @@ tape('gets computation from source', t => {
     `https://api.github.com/repos/MRN-Code/${name}/tarball/v${version}`;
 
   instance._getFromSource(name, version)
-  .then(res => {
-    t.ok(
-      res.name === name && res.version === version,
-      'returns computation definition'
-    );
+    .then(res => {
+      t.ok(
+        res.name === name && res.version === version,
+        'returns computation definition'
+      );
 
-    const args = githubStub.firstCall.args[0];
+      const args = githubStub.firstCall.args[0];
 
-    t.ok(
-      (
-          args.page === 1 &&
-          args.repo === name &&
-          args.user === 'MRN-Code'
-      ),
-      'calls getTags() with right args'
-    );
+      t.ok(
+        args.page === 1 && args.repo === name && args.user === 'MRN-Code',
+        'calls getTags() with right args'
+      );
 
-    t.equal(
-      urlToString(requestStub.firstCall.args[0]),
-      tarballUrl,
-      'requests expected tarball'
-    );
+      t.equal(
+        urlToString(requestStub.firstCall.args[0]),
+        tarballUrl,
+        'requests expected tarball'
+      );
 
-    fs.readdir(TEST_COMPUTATION_PATH, (err, computations) => { // eslint-disable-line
-      if (err) {
-        return t.end(err);
-      }
+      t.ok(installStub.called, 'runs npm install on computation');
+
+      return Promise.all([
+        readdirAsync(TEST_COMPUTATION_PATH),
+        readdirAsync(path.join(TEST_COMPUTATION_PATH, slug)),
+      ]);
+    })
+    .then(([computations, files]) => {
+      const requiredFiles = ['index.js', 'package.json'];
 
       t.ok(
         computations.indexOf(slug) !== -1,
         'unpacks tarball in expected directory'
       );
-
-      fs.readdir(
-        path.join(TEST_COMPUTATION_PATH, slug),
-        (err, files) => {
-          if (err) { return t.end(err); }
-          const requiredFiles = ['index.js', 'package.json'];
-          t.ok(
-            requiredFiles.every(f => files.indexOf(f) !== -1) &&
-            files.every(f => {
-              return (
-                requiredFiles.indexOf(f) !== -1 ||
-                f === 'node_modules'
-              );
-            }),
-            'unpacks all computation files'
+      t.ok(
+        requiredFiles.every(f => files.indexOf(f) !== -1) &&
+        files.every(f => {
+          return (
+            requiredFiles.indexOf(f) !== -1 ||
+            f === 'node_modules'
           );
-
-          return fs.readdir(
-            path.join(TEST_COMPUTATION_PATH, slug, 'node_modules'),
-            (err, nodeModules) => {
-              if (err) { return t.end(err); }
-              t.ok(
-                nodeModules.length === 1 &&
-                nodeModules[0] === 'lodash',
-                'installs dependencies via NPM'
-              );
-              // Cleanup
-              githubStub.restore();
-              return requestStub.restore();
-            }
-          );
-        }
+        }),
+        'unpacks all computation files'
       );
-    });
-  })
-  .then(helpers.cleanupTestDir)
 
-  // Remove temporary test computation directory
-  .then(() => t.pass('test cleanup'))
-  .catch(t.end);
+      // Cleanup
+      githubStub.restore();
+      installStub.restore();
+      requestStub.restore();
+      return helpers.cleanupTestDir();
+    })
+
+    // Remove temporary test computation directory
+    .then(() => t.pass('test cleanup'))
+    .catch(t.end);
 });
 
 tape('handles GitHub API errors', t => {

commit 06cb0fa8d411ce37b190372a2f7d79b5e1af5ab5

Fix computation registry module install regression.
This addresses a regression introduced in cf67e01d7e72b67fd98e7599cd6bd3f2eb172e98
where the `ComputationRegistry`'s retrieval from source passed the `npm install`
command the 'path' module instead of the computation's path. The regression is
addressed and a test is adjusted to better cover the case.

packages/coinstac-common/src/services/classes/computation-registry.js
@@ -295,7 +295,7 @@ class ComputationRegistry {
           tarExtract.on('finish', resolve);
         });
       })
-      .then(() => ComputationRegistry.runNPMInstall(path))
+      .then(() => ComputationRegistry.runNPMInstall(computationPath))
       .then(() => this._getFromDisk(name, version));
   }
 
@@ -423,12 +423,16 @@ class ComputationRegistry {
    * @private
    * @static
    *
-   * @param {string} path
+   * @param {string} computationPath
    * @returns {Promise}
    */
-  static runNPMInstall(path) {
+  static runNPMInstall(computationPath) {
     return new Promise((resolve, reject) => {
-      const child = spawn('npm', ['install', '--production'], { cwd: path });
+      const child = spawn(
+        'npm',
+        ['install', '--production'],
+        { cwd: computationPath }
+      );
 
       let error;
       let out;

packages/coinstac-common/test/services/computation-registry.js
@@ -317,7 +317,12 @@ tape('gets computation from source', t => {
         'requests expected tarball'
       );
 
-      t.ok(installStub.called, 'runs npm install on computation');
+      t.ok(
+        installStub.calledWithExactly(
+          instance._getComputationPath(name, version)
+        ),
+        'runs npm install on computation'
+      );
 
       return Promise.all([
         readdirAsync(TEST_COMPUTATION_PATH),