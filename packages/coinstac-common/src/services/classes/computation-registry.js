bab6d1a8777b8b35f7a6af212a876f7a63306aa5
Cory Reed <creed@mrn.org>
2016-10-13 16:32:54 -0600

    Remove network retrieval from computation registry.
    This drops the network retrieval of computations from GitHub for the computation
    registry. It also drops removal and addition of computations from disk.

--- packages/coinstac-common/src/services/classes/computation-registry.js
+++ packages/coinstac-common/src/services/classes/computation-registry.js
@@ -1,22 +1,10 @@
 'use strict';
 
 const assign = require('lodash/assign');
-const bluebird = require('bluebird');
-const compact = require('lodash/compact');
-const concatStream = require('concat-stream');
 const DecentralizedComputation =
     require('../../models/decentralized-computation.js');
-const followRedirects = require('follow-redirects');
 const fs = require('fs');
-const GitHubApi = require('github');
-const gunzipMaybe = require('gunzip-maybe');
-const mkdirp = require('mkdirp');
 const path = require('path');
-const rimraf = require('rimraf');
-const spawn = require('child_process').spawn;
-const tail = require('lodash/tail');
-const tar = require('tar-fs');
-const url = require('url');
 const values = require('lodash/values');
 
 /**
@@ -49,106 +37,11 @@ function registryFilter(name, version) {
 }
 
 /**
- * Find tarball URL from GitHub API response.
- *
- * The GitHub tag API is poorly documented. Response objects aren’t accurate:
- * {@link https://developer.github.com/v3/git/tags/}
- *
- * {@link http://mikedeboer.github.io/node-github/#repos.prototype.getTags}
- *
- * @todo  The GitHub API paginates tags. This function will need to recursively
- * search pages for the tag. There's no tag count/info, so it's impossible to do
- * a binary search. Figure out something!
- * @private
- * @param {object} options
- * @param {GitHubApi} options.github GitHub API client instance
- * @param {string} options.repo Repository name
- * @param {string} options.user Repository owner
- * @param {string} options.version Tag to find
- * @param {string} [options.page=1] API page to search (1-based index)
- * @param {function} callback
- */
-function findTarballUrl(options, callback) {
-  const github = options.github;
-  const repo = options.repo;
-  const user = options.user;
-  const version = options.version;
-  const page = typeof options.page !== 'undefined' ? options.page : 1;
-
-  github.repos.getTags({
-    page,
-    per_page: 100,
-    repo,
-    user,
-  }, (err, res) => {
-    if (err) { return callback(err); }
-
-    const tag = res.find(t => t.name === `v${version}`);
-
-    // Potentially recurse here
-    if (!tag) {
-      return callback(new Error(
-        `Couldn’t find tag for ${user}/${repo}@${version}`
-      ));
-    }
-    return callback(null, tag.tarball_url); // jshint ignore:line
-  });
-}
-
-/**
- * Get a computation's tarball URL.
- * @private
- * @param {object} options
- * @param {GitHubApi} options.github GitHub API client instance
- * @param {string} options.name
- * @param {object[]} options.registry
- * @param {string} options.version
- * @returns {Promise}
- */
-function getTarballUrl(options) {
-  const github = options.github;
-  const name = options.name;
-  const registry = options.registry;
-  const version = options.version;
-
-  const registryItem = registry.find(registryFilter(name, version));
-
-  if (!registryItem) {
-    return Promise.reject(new Error(
-            `Couldn’t find URL for ${getId(name, version)}`
-        ));
-  }
-
-    /**
-     * @todo  This expects `registryItem.url` to be a GitHub URL. Expand to be
-     * more flexible.
-     */
-  const pathPieces = compact(url.parse(registryItem.url).path.split('/'));
-
-  return new Promise((resolve, reject) => {
-    findTarballUrl({
-      github,
-      repo: pathPieces[1],
-      user: pathPieces[0],
-      version,
-    }, (err, tarballUrl) => {
-      if (err) {
-        return reject(err);
-      }
-
-      return resolve(tarballUrl);
-    });
-  });
-}
-
-
-/**
  * Computation registry.
  * @class
  *
  * @example
  * const instance = new ComputationRegistry({
- *   github: new GitHubApi({...}),
  *   path: path.join(__dirname, 'computations'),
  *   registry: [{
  *     name: 'my-computation',
@@ -162,16 +55,11 @@ function getTarballUrl(options) {
  * });
  *
  * @param {object} options
- * @param {GitHubApi} options.github GitHub API client instance
  * @param {string} options.path Path on disk to cache computations
  * @param {object[]} options.registry Collection of computation registry objects
  */
 class ComputationRegistry {
   constructor(options) {
-    if (!options.github || !(options.github instanceof GitHubApi)) {
-      throw new TypeError('Expected GitHubApi instance');
-    }
-
     if (!options.path) {
       throw new TypeError('Expected path');
     }
@@ -180,7 +68,6 @@ class ComputationRegistry {
       throw new TypeError('Expected registry of computations');
     }
 
-    this.github = options.github;
     this.path = options.path;
     this.registry = options.registry;
     this.store = {};
@@ -248,58 +135,6 @@ class ComputationRegistry {
   }
 
   /**
-   * Get a computation definition from source (the web).
-   *
-   * This has a side effect of saving the computation definition to disk.
-   * @private
-   * @param {string} name
-   * @param {string} version
-   * @returns {Promise}
-   */
-  _getFromSource(name, version) {
-    const computationPath = this._getComputationPath(name, version);
-
-    return Promise.all([
-      getTarballUrl({
-        github: this.github,
-        name,
-        registry: this.registry,
-        version,
-      }),
-      bluebird.promisify(mkdirp)(computationPath),
-    ])
-      .then(([tarballUrl]) => {
-        const parsedUrl = url.parse(tarballUrl);
-        const tarExtract = tar.extract(computationPath, {
-          map: header => {
-            // Ensure the tarball isn't unpacked into a deep directory
-            // TODO: Add test to make sure this is needed
-            header.name = tail(header.name.split('/')).join('/');
-
-            return header;
-          },
-        });
-
-        return new Promise((resolve, reject) => {
-          followRedirects.https.get({
-            hostname: parsedUrl.hostname,
-            path: parsedUrl.path,
-            headers: {
-              'User-Agent': 'COINSTAC',
-            },
-            protocol: parsedUrl.protocol,
-          }, res => {
-            res.pipe(gunzipMaybe()).pipe(tarExtract);
-          }).on('error', reject);
-          tarExtract.on('error', reject);
-          tarExtract.on('finish', resolve);
-        });
-      })
-      .then(() => ComputationRegistry.runNPMInstall(computationPath))
-      .then(() => this._getFromDisk(name, version));
-  }
-
-  /**
    * Add a decentralized computation to the store.
    *
    * @param {string} name
@@ -308,15 +143,6 @@ class ComputationRegistry {
    */
   add(name, version) {
     const registryItem = this.registry.find(registryFilter(name, version));
-
-    function doAdd(definition) {
-      return this._doAdd({
-        definition,
-        name,
-        url: registryItem.url,
-        version,
-      });
-    }
 
     // Check registry to make sure name/version is valid
     if (!registryItem) {
@@ -329,14 +155,13 @@ class ComputationRegistry {
     return this.get(name, version)
 
       // Check disk to see if computation is saved
-      .catch(() => {
-        return this._getFromDisk(name, version).then(doAdd.bind(this));
-      })
-
-      // Download computation from source, save to Disk
-      .catch(() => {
-        return this._getFromSource(name, version).then(doAdd.bind(this));
-      });
+      .catch(() => this._getFromDisk(name, version))
+      .then(definition => this._doAdd({
+        definition,
+        name,
+        url: registryItem.url,
+        version,
+      }));
   }
 
   /**
@@ -372,37 +197,6 @@ class ComputationRegistry {
   }
 
   /**
-   * Remove a computation.
-   *
-   * @param {string} name
-   * @param {string} version
-   * @param {boolean} [fromDisk=false] Remove cached computation from disk
-   * @returns {Promise}
-   */
-  remove(name, version, fromDisk) {
-    const id = getId(name, version);
-
-    if (!(id in this.store)) {
-      return Promise.reject(new Error(
-              `Computation ${id} not in registry`
-          ));
-    }
-
-    delete this.store[id];
-
-    if (!fromDisk) {
-      return Promise.resolve();
-    }
-
-    return new Promise((resolve, reject) => {
-      rimraf(this._getComputationPath(name, version), (err, res) => {
-        if (err) { return reject(err); }
-        return resolve(res);
-      });
-    });
-  }
-
-  /**
    * Get a computation's name and version from its directory.
    *
    * @todo  Test it
@@ -416,42 +210,6 @@ class ComputationRegistry {
       return [matches[1], matches[2]];
     }
     return null;
-  }
-
-  /**
-   * Run `npm install` on a path.
-   * @private
-   * @static
-   *
-   * @param {string} computationPath
-   * @returns {Promise}
-   */
-  static runNPMInstall(computationPath) {
-    return new Promise((resolve, reject) => {
-      const child = spawn(
-        'npm',
-        ['install', '--production'],
-        { cwd: computationPath }
-      );
-
-      let error;
-      let out;
-
-      child.stderr.pipe(concatStream(data => {
-        error = data.toString();
-      }));
-      child.stdout.pipe(concatStream(data => {
-        out = data.toString();
-      }));
-
-      child.on('close', exitCode => {
-        /* istanbul ignore if */
-        if (exitCode) {
-          return reject(error);
-        }
-        return resolve(out);
-      });
-    });
   }
 }
 
