fb488e66a37f2d9e0bc4191a6b08a3cb3f6cb580
Ross Kelly <rkelly@mrn.org>
2016-10-27 17:35:27 -0600

    remove comments/unused package + add used package:-)

--- packages/coinstac-ui/scripts/build-setup.js
+++ packages/coinstac-ui/scripts/build-setup.js
@@ -28,12 +28,5 @@ rmrf('./node_modules/coinstac-*(common|client-core)')
 })
 .then(() => {
     return ncp('../coinstac-client-core/package.json', './node_modules/coinstac-client-core/package.json');
-})
-// rm -rf node_modules/coinstac-{common,client-core}
-// mkdir -p node_modules/coinstac-{common,client-core}
-
-// cp -r ../coinstac-common/{package.json,src} ./node_modules/coinstac-common/
-// cp -r ../coinstac-client-core/{bin,config.js,package.json,src} ./node_modules/coinstac-client-core/
-
-// npm install
+});
 
