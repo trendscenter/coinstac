68be7e74618d55f6ac791ca23204facbf405ab1f
Cory Reed <creed@mrn.org>
2017-03-28 10:31:17 -0700

    Add rounding to numeric computation input fields.

--- packages/coinstac-ui/app/render/components/consortium/consortium-computation-fields.js
+++ packages/coinstac-ui/app/render/components/consortium/consortium-computation-fields.js
@@ -1,8 +1,19 @@
 import React, { PropTypes } from 'react';
 import { Field, FieldArray } from 'redux-form';
+import { round } from 'lodash';
 
 import ComputationFieldBasic from '../computation-field-basic';
 import ComputationFieldCovariates from '../computation-field-covariates';
+
+/**
+ * Normalize numeric input.
+ *
+ * {@link http://redux-form.com/6.6.0/examples/normalizing/}
+ *
+ * @param {number} value
+ * @returns {number} Normalized value
+ */
+const normalizer = value => round(value, 3);
 
 /**
  * Consortium computation fields.
@@ -19,18 +30,28 @@ export default function ConsortiumComputationFields({
   return (
     <ol className="list-unstyled">
       {fields.map((name, index) => {
-        const field = computationInputs[index];
-        const component = field.type === 'covariates' ?
-          <FieldArray
-            component={ComputationFieldCovariates}
-            name={name}
-            {...field}
-          /> :
-          <Field
-            component={ComputationFieldBasic}
-            name={name}
-            {...field}
-          />;
+        const fieldProps = Object.assign({ name }, computationInputs[index]);
+        let component;
+
+        if (fieldProps.type === 'covariates') {
+          component = (
+            <FieldArray
+              component={ComputationFieldCovariates}
+              {...fieldProps}
+            />
+          );
+        } else {
+          if (fieldProps.type === 'number') {
+            fieldProps.normalize = normalizer;
+          }
+
+          component = (
+            <Field
+              component={ComputationFieldBasic}
+              {...fieldProps}
+            />
+          );
+        }
 
         return <li key={index}>{component}</li>;
       })}
