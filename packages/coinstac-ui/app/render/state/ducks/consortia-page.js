99bcd87b7f5c3a531c31e87f17552ff7894f5c39
Torran Kahleck <torrankahleck@torrans-mbp.mind.unm.edu>
2017-05-22 14:59:26 -0600

    initialized expandedComputation to an empty string

--- packages/coinstac-ui/app/render/state/ducks/consortia-page.js
+++ packages/coinstac-ui/app/render/state/ducks/consortia-page.js
@@ -3,18 +3,18 @@ const SET_EXPANDED_COMPUTATION = 'SET_EXPANDED_COMPUTATION';
 export const setExpandedComputation = (computationId) => {
   return {
     type: SET_EXPANDED_COMPUTATION,
-    payload: computationId
+    payload: computationId,
   };
 };
 
 const INITIAL_STATE = {
-  expandedComputation: 'sfkfsdlfm',
+  expandedComputation: '',
 };
 
 export default function reducer(state = INITIAL_STATE, action) {
   switch (action.type) {
     case SET_EXPANDED_COMPUTATION:
-      return {...state, expandedComputation: action.payload};
+      return { expandedComputation: action.payload };
     default:
       return state;
   }
