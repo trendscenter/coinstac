8f82f143d29455b0a0ad9216266201f93815e3e0
tkah <tkahleck@unm.edu>
2017-06-26 14:29:43 -0600

    implicit return for all

--- packages/coinstac-ui/test/e2e/test.js
+++ packages/coinstac-ui/test/e2e/test.js
@@ -32,35 +32,35 @@ describe('Testing::e2e', () => {
     }
   });
 
-  it('opens a single window', () => {
-    return app.client.waitUntilWindowLoaded()
-      .getWindowCount().should.eventually.equal(1);
-  });
+  it('opens a single window', () =>
+    app.client.waitUntilWindowLoaded()
+      .getWindowCount().should.eventually.equal(1)
+  );
 
-  it('displays the correct title', () => {
-    return app.client.waitUntilWindowLoaded()
-      .getTitle().should.eventually.equal('COINSTAC');
-  });
+  it('displays the correct title', () =>
+    app.client.waitUntilWindowLoaded()
+      .getTitle().should.eventually.equal('COINSTAC')
+  );
 
-  it('authenticates demo user', () => {
-    return app.client
+  it('authenticates demo user', () =>
+    app.client
       .setValue('#login-username', USER_ID)
       .setValue('#login-password', PASS)
       .click('button=Log In')
       .waitForExist('.user-account-name', EXIST_TIMEOUT)
-      .getText('.user-account-name').should.eventually.equal(USER_ID);
-  });
+      .getText('.user-account-name').should.eventually.equal(USER_ID)
+  );
 
-  it('accesses the Add Consortium page', () => {
-    return app.client
+  it('accesses the Add Consortium page', () =>
+    app.client
       .click('a=Consortia')
       .waitForExist('a=Add Consortium', EXIST_TIMEOUT)
       .click('a=Add Consortium')
-      .isVisible('h1=Add New Consortium').should.eventually.equal(true);
-  });
+      .isVisible('h1=Add New Consortium').should.eventually.equal(true)
+  );
 
-  it('creates a consortium', () => {
-    return app.client
+  it('creates a consortium', () =>
+    app.client
       .setValue("[name='label']", CONS_NAME)
       .setValue("[name='description']", CONS_DESC)
       .click('p=Decentralized ridge regression.')
@@ -79,19 +79,19 @@ describe('Testing::e2e', () => {
       .selectByValue('#computation-field-map-type-1', 'number')
       .click('button=Create')
       .waitForExist('div.panel-heading h4.panel-title', EXIST_TIMEOUT)
-      .getText('div.panel-heading h4.panel-title a').should.eventually.equal(CONS_NAME);
-  });
+      .getText('div.panel-heading h4.panel-title a').should.eventually.equal(CONS_NAME)
+  );
 
-  it('accesses the New Files Collection page', () => {
-    return app.client
+  it('accesses the New Files Collection page', () =>
+    app.client
       .click('a=My Files')
       .waitForExist('a=Add Files Collection', EXIST_TIMEOUT)
       .click('a=Add Files Collection')
-      .isVisible('h1=New Files Collection').should.eventually.equal(true);
-  });
+      .isVisible('h1=New Files Collection').should.eventually.equal(true)
+  );
 
-  it('adds files to a consortium', () => {
-    return app.client
+  it('adds files to a consortium', () =>
+    app.client
       .setValue('#form-project-name', PROJECT_NAME)
       .selectByVisibleText('#form-project-consortium-id', CONS_NAME)
       .click('button=Add File')
@@ -100,41 +100,41 @@ describe('Testing::e2e', () => {
       .selectByValue('#project-covariates-mapper-2', '1')
       .click('button=Save')
       .waitForExist('div.panel-heading h4.panel-title', EXIST_TIMEOUT)
-      .getText('div.panel-heading h4.panel-title a').should.eventually.equal(PROJECT_NAME);
-  });
+      .getText('div.panel-heading h4.panel-title a').should.eventually.equal(PROJECT_NAME)
+  );
 
-  it('runs a computation', () => {
-    return app.client
+  it('runs a computation', () =>
+    app.client
       .click('button=Run Computation')
-      .waitForExist('span=Complete', EXIST_TIMEOUT);
-  });
+      .waitForExist('span=Complete', EXIST_TIMEOUT)
+  );
 
-  it('displays results', () => {
-    return app.client
+  it('displays results', () =>
+    app.client
       .click('a=View Results')
       .waitForExist('#results', EXIST_TIMEOUT)
-      .waitForExist('span=Complete!', EXIST_TIMEOUT);
-  });
+      .waitForExist('span=Complete!', EXIST_TIMEOUT)
+  );
 
-  it('deletes files', () => {
-    return app.client
+  it('deletes files', () =>
+    app.client
       .click('a=My Files')
       .waitForExist('a=Add Files Collection', EXIST_TIMEOUT)
       .click('button=Delete')
-      .isVisible('div.panel-heading').should.eventually.equal(false);
-  });
+      .isVisible('div.panel-heading').should.eventually.equal(false)
+  );
 
-  it('deletes a consortium', () => {
-    return app.client
+  it('deletes a consortium', () =>
+    app.client
       .click('a=Consortia')
       .waitForExist('a=Add Consortium', EXIST_TIMEOUT)
       .click('button=Delete')
-      .isVisible('div.panel-heading').should.eventually.equal(false);
-  });
+      .isVisible('div.panel-heading').should.eventually.equal(false)
+  );
 
-  it('logs out', () => {
-    return app.client
+  it('logs out', () =>
+    app.client
       .click('button=Log Out')
-      .waitForExist('button=Log In', EXIST_TIMEOUT);
-  });
+      .waitForExist('button=Log In', EXIST_TIMEOUT)
+  );
 });
