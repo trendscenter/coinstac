86865a3b2da5405e143cabff9a6bf5a456367ddc
tkah <tkahleck@unm.edu>
2017-07-11 12:07:20 -0600

    addressing first review concerns

--- packages/coinstac-ui/app/render/containers/root.jsx
+++ packages/coinstac-ui/app/render/containers/root.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
-import { Router, hashHistory } from 'react-router';
+import { Router } from 'react-router';
 
-import configureStore from '../state/store';
 import routes from '../routes';
 
-const store = configureStore();
-
-const Root = () => (
+const Root = ({ history, store }) => (
   <Provider store={store}>
-    <Router history={hashHistory} routes={routes} />
+    <Router history={history} routes={routes} />
   </Provider>
 );
+
+Root.propTypes = {
+  history: PropTypes.object.isRequired,
+  store: PropTypes.object.isRequired,
+};
 
 export default Root;
