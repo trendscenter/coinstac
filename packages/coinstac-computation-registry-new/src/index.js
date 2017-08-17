e240f61a3c80939a58e21a183d30934701e7de15
tkah <tkahleck@unm.edu>
2017-08-17 12:19:23 -0600

    removed local whitelist references, preparing for DB calls

--- packages/coinstac-computation-registry-new/src/index.js
+++ packages/coinstac-computation-registry-new/src/index.js
@@ -1,15 +1,8 @@
 'use strict';
 
-const nano = require('nano')('http://coinstac:test@localhost:5984');
-const { spawn, spawnSync } = require('child_process');
-const fs = require('fs');
-const { compact } = require('lodash');
 const CLIAdapter = require('./adapters/cli-adapter');
+const TestAdapter = require('./adapters/test-adapter');
 const UIAdapter = require('./adapters/ui-adapter');
-const whitelist = require('./whitelisted-computations');
-
-const DIR_TO_COPY = '/etc/';
-const FILE_TO_COPY = 'adduser.conf';
 
 /**
  * ComputationRegistry
@@ -23,19 +16,19 @@ class ComputationRegistry {
     } else {
       this.adapter = new UIAdapter();
     }
-
-    nano.db.create('comp-schema', (err, body) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log(body);
-      }
-    });
   }
 
   /**
    * Client
    */
+
+  getAllComputations() {
+    // TODO: Return IDs, image name and comp names from all computations
+  }
+
+  getMetadataForName(imageName) {
+    // TODO: Return all metadata for given image name
+  }
 
   /**
    * Generate array of docker pull promises and wait until aa resolved to return
@@ -55,91 +48,8 @@ class ComputationRegistry {
     .catch(console.log);
   }
 
-  getLocalImages() { // eslint-disable-line class-methods-use-this
-    return new Promise((res) => {
-      const sp = spawn('docker', ['ps', '--format', '{{.ID}} {{.Image}}']);
-      let containerImages = [];
-
-      sp.stdout.on('data', (data) => {
-        let dataIn = compact(data.toString().split('\n'));
-        dataIn = dataIn.map(row => row.split(/\s+/));
-        containerImages = containerImages.concat(dataIn);
-      });
-
-      sp.stderr.on('data', (data) => {
-        res(data);
-      });
-
-      sp.on('close', () => {
-        res(containerImages);
-      });
-    })
-    .then((containerImages) => {
-      return new Promise((res) => {
-        const sp = spawn('docker', ['images', '--format', '{{.ID}} {{.Repository}} {{.Tag}} {{.Size}}']);
-        let images = [];
-
-        sp.stdout.on('data', (data) => {
-          let dataIn = compact(data.toString().split('\n'));
-          dataIn = dataIn.map((row) => {
-            const imgSplit = row.split(/\s+/);
-            const imgIndex = containerImages.findIndex(img => imgSplit[1] === img[1]);
-            if (imgIndex > -1) {
-              imgSplit.push(containerImages[imgIndex][0]);
-            }
-            return imgSplit;
-          });
-          images = images.concat(dataIn);
-        });
-
-        sp.stderr.on('data', (data) => {
-          res(data);
-        });
-
-        sp.on('close', () => {
-          res(images);
-        });
-      });
-    })
-    .catch(console.log);
-  }
-
-  removeContainer(containerId) {  // eslint-disable-line class-methods-use-this
-    return new Promise((res) => {
-      const sp = spawn('docker', ['rm', '-f', containerId]);
-      const outArr = [];
-
-      sp.stdout.on('data', (data) => {
-        outArr.push(data);
-      });
-
-      sp.stderr.on('data', (data) => {
-        res(data);
-      });
-
-      sp.on('close', () => {
-        res(outArr);
-      });
-    });
-  }
-
-  removeImage(imageId) {  // eslint-disable-line class-methods-use-this
-    return new Promise((res) => {
-      const sp = spawn('docker', ['rmi', imageId]);
-      const outArr = [];
-
-      sp.stdout.on('data', (data) => {
-        outArr.push(data);
-      });
-
-      sp.stderr.on('data', (data) => {
-        res(data);
-      });
-
-      sp.on('close', () => {
-        res(outArr);
-      });
-    });
+  validateComputation(id) {
+    // TODO: Return boolean for whether or not computation is approved
   }
 
   /**
@@ -147,41 +57,19 @@ class ComputationRegistry {
    */
 
   /**
-   * Recursive function to create temporary container with first image in passed in array,
-   *   copy contents of metadata file locally, then push that metadata information to the DB
-   * @param {Array} comps - list of computation images
+   * Submit computation schema for approval
+   * @param {Object} comp - authors computation schema
    */
-  addMetadataToDB(comps) { // eslint-disable-line class-methods-use-this
-    if (comps.length === 0) {
-      fs.rmdirSync(`${__dirname}/tmp`);
-      return 'finished';
-    }
+  addComputation(comp) { // eslint-disable-line class-methods-use-this
+    // TODO: Insert passed-in computation schema to DB
+  }
 
-    spawnSync('docker', [
-      'run',
-      '--rm',
-      '-v',
-      `${__dirname}/tmp:/tmp`,
-      comps[0],
-      'sh',
-      '-c',
-      `cp -r ${DIR_TO_COPY}${FILE_TO_COPY} /tmp`,
-    ], { stdio: 'inherit' });
-
-    const metadata = fs.readFileSync(`${__dirname}/tmp/${FILE_TO_COPY}`);
-
-    const compSchema = nano.use('comp-schema');
-    compSchema.insert({ metadata: metadata.toString() }, comps.shift(), (err, body) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log(body);
-      }
-    });
-
-    fs.unlinkSync(`${__dirname}/tmp/${FILE_TO_COPY}`);
-
-    this.addMetadataToDB(comps);
+  /**
+   * Remove computation schema
+   * @param {int} id - authors computation schema
+   */
+  removeComputation(id) { // eslint-disable-line class-methods-use-this
+    // TODO: Remove passed-in computation id from DB
   }
 
   /**
@@ -191,16 +79,8 @@ class ComputationRegistry {
    * @return {Promise<string>} Success flag
    */
   serverStart() {
-    const comps = whitelist.reduce((arr, comp) => {
-      arr.push(comp.dockerImage);
-      return arr;
-    }, []);
-
-    return this.pullPipelineComputations({ comps })
-    .then(() => new Promise((res) => {
-      this.addMetadataToDB(comps);
-      res();
-    }));
+    // TODO: comps = DB Pull of approved computations
+    return this.pullPipelineComputations({ comps });
   }
 }
 
