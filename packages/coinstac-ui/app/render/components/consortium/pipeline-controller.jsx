548bbf65f310725765cebcb1f7b14b2a967d7375
tkah <tkahleck@unm.edu>
2017-09-26 15:44:18 -0600

    nested dnd lists working

--- packages/coinstac-ui/app/render/components/consortium/pipeline-controller.jsx
+++ packages/coinstac-ui/app/render/components/consortium/pipeline-controller.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { DragSource, DropTarget } from 'react-dnd';
 import PropTypes from 'prop-types';
 import { compose } from 'redux';
+import { Button } from 'react-bootstrap';
 import ItemTypes from './pipeline-item-types';
+import PipelineComputation from './pipeline-computation';
 
 const style = {
   border: '1px dashed gray',
@@ -49,29 +51,57 @@ const collectDrag = (connect, monitor) => (
   }
 );
 
-const collectDrop = (connect, monitor) => (
-  {
-    connectDropTarget: connect.dropTarget(),
-  }
-);
+const collectDrop = connect =>
+  ({ connectDropTarget: connect.dropTarget() });
 
-const PipelineController = ({ controller, isDragging, connectDragSource, connectDropTarget }) =>
+const PipelineController = (
+  {
+    addComputation,
+    controller,
+    isDragging,
+    connectDragSource,
+    connectDropTarget,
+    controllerIndex,
+    moveComputation,
+  }
+) =>
   connectDragSource(connectDropTarget(
     <div style={{ ...style, opacity: isDragging ? 0 : 1 }}>
       <p style={{ marginTop: 0, fontWeight: 'bold' }}>{controller.options.iterations}</p>
+      <Button
+        bsStyle="primary"
+        type="button"
+        onClick={() => addComputation(controllerIndex)}
+      >
+        <span aria-hidden="true" className="glphicon glyphicon-plus" />
+        {' '}
+        Add Computation
+      </Button>
+      {controller.computations &&
+        controller.computations.map(computation => (
+          <PipelineComputation
+            id={computation.id}
+            key={computation.id}
+            computation={computation}
+            controllerIndex={controllerIndex}
+            moveComputation={moveComputation}
+          />
+        ))
+      }
     </div>
   ));
 
 PipelineController.propTypes = {
   connectDragSource: PropTypes.func.isRequired,
   connectDropTarget: PropTypes.func.isRequired,
+  id: PropTypes.string.isRequired,
   isDragging: PropTypes.bool.isRequired,
-  id: PropTypes.any.isRequired,
   controller: PropTypes.object.isRequired,
   moveController: PropTypes.func.isRequired,
+  moveComputation: PropTypes.func.isRequired,
 };
 
 export default compose(
-  DropTarget(ItemTypes.COMPUTATION, controllerTarget, collectDrop),
-  DragSource(ItemTypes.COMPUTATION, controllerSource, collectDrag)
+  DropTarget(ItemTypes.CONTROLLER, controllerTarget, collectDrop),
+  DragSource(ItemTypes.CONTROLLER, controllerSource, collectDrag)
 )(PipelineController);
