6785a30e433a15fa26ae22544c98fd96ae688e2a
tkah <tkahleck@unm.edu>
2017-10-18 16:30:41 -0600

    starting test collection

--- packages/coinstac-ui/app/render/state/ducks/collections.js
+++ packages/coinstac-ui/app/render/state/ducks/collections.js
@@ -1,13 +1,15 @@
 import { applyAsyncLoading } from './loading';
 import localDB from '../local-db';
+import testData from '../../../../test/data/test-collection';
 
 // Actions
 const SET_COLLECTIONS = 'GET_ALL_COLLECTIONS';
+const INIT_TEST_DATA = 'INIT_TEST_DATA';
 
 // Action Creators
 export const getAllCollections = applyAsyncLoading(() =>
   dispatch =>
-    localDB.table('collections')
+    localDB.collections
       .toArray()
       .then((collections) => {
         dispatch(({
@@ -17,12 +19,25 @@ export const getAllCollections = applyAsyncLoading(() =>
       })
 );
 
+export const initTestData = (() =>
+  dispatch =>
+    localDB.collections.put(testData, 'test-collection')
+    .then(() => {
+      dispatch(({
+        type: INIT_TEST_DATA,
+        payload: null,
+      }));
+    })
+);
+
 const INITIAL_STATE = {
   collections: [],
 };
 
 export default function reducer(state = INITIAL_STATE, action) {
   switch (action.type) {
+    case INIT_TEST_DATA:
+      return { collections: [...state.collections, action.payload] };
     case SET_COLLECTIONS:
       return { collections: action.payload };
     default:
