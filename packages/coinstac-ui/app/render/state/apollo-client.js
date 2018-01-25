2a591709cdcf641b74a2522350bb492f67b622d0
tkah <tkahleck@unm.edu>
2018-01-25 12:11:46 -0700

    pass global config into client, use remote to get config in auth

--- packages/coinstac-ui/app/render/state/apollo-client.js
+++ packages/coinstac-ui/app/render/state/apollo-client.js
@@ -5,50 +5,45 @@ import {
 } from 'react-apollo';
 import { SubscriptionClient, addGraphQLSubscriptions } from 'subscriptions-transport-ws';
 
-const loadConfig = require('../../config.js');
+export default function getApolloClient(config) {
+  const { apiServer, subApiServer } = config.getProperties();
+  const API_URL = `${apiServer.protocol}//${apiServer.hostname}:${apiServer.port}`;
+  const networkInterface = createBatchingNetworkInterface({
+    uri: `${API_URL}/graphql`,
+    batchInterval: 10,
+  });
 
-export default function getApolloClient() {
-  return loadConfig()
-    .then((config) => {
-      const { apiServer, subApiServer } = config.getProperties();
-      const API_URL = `${apiServer.protocol}//${apiServer.hostname}:${apiServer.port}`;
-      const networkInterface = createBatchingNetworkInterface({
-        uri: `${API_URL}/graphql`,
-        batchInterval: 10,
-      });
+  const SUB_URL = `${subApiServer.protocol}//${subApiServer.hostname}:${subApiServer.port}`;
+  const wsClient = new SubscriptionClient(`${SUB_URL}/subscriptions`, { reconnect: true });
+  const networkInterfaceWithSubscriptions = addGraphQLSubscriptions(
+    networkInterface,
+    wsClient
+  );
 
-      const SUB_URL = `${subApiServer.protocol}//${subApiServer.hostname}:${subApiServer.port}`;
-      const wsClient = new SubscriptionClient(`${SUB_URL}/subscriptions`, { reconnect: true });
-      const networkInterfaceWithSubscriptions = addGraphQLSubscriptions(
-        networkInterface,
-        wsClient
-      );
+  const client = new ApolloClient({
+    networkInterface: networkInterfaceWithSubscriptions,
+    queryTransformer: addTypeName,
+    dataIdFromObject: o => o.id,
+  });
 
-      const client = new ApolloClient({
-        networkInterface: networkInterfaceWithSubscriptions,
-        queryTransformer: addTypeName,
-        dataIdFromObject: o => o.id,
-      });
+  client.networkInterface.use([{
+    applyBatchMiddleware(req, next) {
+      if (!req.options.headers) {
+        req.options.headers = {};  // Create the header object if needed.
+      }
 
-      client.networkInterface.use([{
-        applyBatchMiddleware(req, next) {
-          if (!req.options.headers) {
-            req.options.headers = {};  // Create the header object if needed.
-          }
+      // get the authentication token from local storage if it exists
+      let token = localStorage.getItem('id_token');
 
-          // get the authentication token from local storage if it exists
-          let token = localStorage.getItem('id_token');
+      if (!token || token === 'null' || token === 'undefined') {
+        token = sessionStorage.getItem('id_token');
+      }
 
-          if (!token || token === 'null' || token === 'undefined') {
-            token = sessionStorage.getItem('id_token');
-          }
+      req.options.headers.authorization = token ? `Bearer ${token}` : null;
+      next();
+    },
+  }]);
 
-          req.options.headers.authorization = token ? `Bearer ${token}` : null;
-          next();
-        },
-      }]);
-
-      return client;
-    });
+  return client;
 }
 
