b46adfdb5e1cc51ff669a4bd196635c26b5382d3
Ross Kelly <rkelly@mrn.org>
2018-02-05 15:23:43 -0700

    add sim test code for pipeline

---
+++ packages/coinstac-simulator/test/test-comps/coinstac-decentralized-test/remote.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import sys
+import json
+
+doc = json.loads(sys.argv[1])
+sums = 0
+for site, output in doc['input'].items():
+    sums = sums + output['sum'];
+sums = sums / len(doc['input'])
+if sums > 4:
+    success = True
+else:
+    success = False
+
+output = { "output": { "sum": sums }, "success": success }
+sys.stdout.write(json.dumps(output))
