1677d94f82b95ddf355547c13e755a8a45bede1e
tkah <tkahleck@unm.edu>
2018-03-19 15:51:12 -0600

    closes issues 440, 437, 432, 430

--- packages/coinstac-ui/app/render/components/dashboard/dashboard.jsx
+++ packages/coinstac-ui/app/render/components/dashboard/dashboard.jsx
@@ -54,8 +54,8 @@ class Dashboard extends Component {
     const { router } = this.context;
 
     // Uncomment to clear local data
-    this.props.clearRuns();
-    this.props.initTestData();
+    // this.props.clearRuns();
+    // this.props.initTestData();
 
     process.nextTick(() => {
       if (!user.email.length) {
@@ -187,6 +187,12 @@ class Dashboard extends Component {
               this.props.incrementRunCount(consortium.id);
               this.props.notifyInfo({
                 message: `Decentralized Pipeline Starting for ${consortium.name}.`,
+                action: {
+                  label: 'Watch Progress',
+                  callback: () => {
+                    router.push('dashboard');
+                  },
+                },
               });
 
               // 5 second timeout to ensure no port conflicts in
