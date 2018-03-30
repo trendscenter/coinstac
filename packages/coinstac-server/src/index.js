949dca59b6afe44378279592c9038e8f17de746c
tkah <tkahleck@unm.edu>
2018-03-30 16:47:05 -0600

    no more comp reg, catching docker promise errors for comp pulls

--- packages/coinstac-server/src/index.js
+++ packages/coinstac-server/src/index.js
@@ -1,12 +1,46 @@
 const hapi = require('hapi');
-const CoinstacComputationRegistry = require('coinstac-computation-registry');
+const axios = require('axios');
+const { pullImages } = require('coinstac-docker-manager');
+const graphqlSchema = require('coinstac-graphql-schema');
 const dbmap = require('/etc/coinstac/cstacDBMap'); // eslint-disable-line import/no-absolute-path, import/no-unresolved
 const config = require('../config/default');
 const routes = require('./routes');
 
-// Download computations on remote server on server start
-const computationRegistry = new CoinstacComputationRegistry({ credentials: dbmap.rethinkdbServer });
-computationRegistry.serverStart();
+let idToken = '';
+
+/**
+ * On server start, pull in comps from DB whitelist and download via docker
+ * @return {Promise<string>} Success flag
+ */
+axios.post(
+  `${config.apiServer}/authenticate`,
+  dbmap.rethinkdbServer
+)
+.then((token) => {
+  idToken = token.data.id_token;
+  axios.defaults.headers.common.Authorization = `Bearer ${idToken}`;
+})
+.then(() =>
+  axios.get(`${config.apiServer}/graphql?query=${graphqlSchema.queries.allDockerImages}`)
+)
+.then(({ data: { data: { fetchAllComputations } } }) => {
+  const comps = fetchAllComputations.map(comp => ({
+    img: comp.computation.dockerImage,
+    compId: comp.id,
+    compName: comp.meta.name,
+  }));
+  return pullImages(comps);
+})
+.then((pullStreams) => {
+  pullStreams.forEach((obj) => {
+    if (typeof obj.stream.pipe === 'function') {
+      obj.stream.pipe(process.stdout);
+    } else {
+      console.log(obj); // eslint-disable-line no-console
+    }
+  });
+})
+.catch(console.log); // eslint-disable-line no-console
 
 const server = new hapi.Server();
 server.connection({
