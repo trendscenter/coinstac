29c101c523123ef1f7364475965ee845c7f26b56
Ross Kelly <rkelly@mrn.org>
2018-09-14 10:42:44 -0600

    add correct logging and put config back

--- packages/coinstac-client-core/src/index.js
+++ packages/coinstac-client-core/src/index.js
@@ -16,14 +16,14 @@ bluebird.config({ warnings: false });
 const osHomedir = require('os-homedir');
 const path = require('path');
 const winston = require('winston');
+// set w/ config etc post release
+process.LOGLEVEL = 'silly';
 
 const Logger = winston.Logger;
 const Console = winston.transports.Console;
 const DockerManager = require('coinstac-docker-manager');
 const PipelineManager = require('coinstac-pipeline');
 
-// set w/ config etc post release
-process.LOGLEVEL = 'silly';
 
 /**
  * Create a user client for COINSTAC
