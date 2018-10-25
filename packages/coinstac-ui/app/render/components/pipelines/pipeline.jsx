b7a35e1312beacb2c78ffa54d8db976c06ae1000
Eduardo Rocha <eduardorochacg@gmail.com>
2018-10-25 15:25:38 -0300

    Fix bug on pipeline details page

--- packages/coinstac-ui/app/render/components/pipelines/pipeline.jsx
+++ packages/coinstac-ui/app/render/components/pipelines/pipeline.jsx
@@ -136,7 +136,10 @@ class Pipeline extends Component {
     const consortiumId = this.state.pipeline.owningConsortium;
     const data = client.readQuery({ query: FETCH_ALL_CONSORTIA_QUERY });
     const consortium = data.fetchAllConsortia.find(cons => cons.id === consortiumId);
-    delete consortium.__typename;
+
+    if (consortium) {
+      delete consortium.__typename;
+    }
 
     this.setState(prevState => ({
       owner,
