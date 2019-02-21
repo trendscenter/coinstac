49db179fd23d59ad74034d342c212ec6e4114a44
Javier Romero <javiertomasromero@gmail.com>
2019-02-21 11:26:36 -0700

    creating custom material-ui palette making some styling tweaks in various places

---
+++ packages/coinstac-ui/app/render/styles/material-ui/theme.js
@@ -0,0 +1,9 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const palette = {
+  primary: { main: '#3F6D87' },
+  secondary: { main: '#f05a29' },
+};
+const themeName = 'Coinstac';
+
+export default createMuiTheme({ palette, themeName });
