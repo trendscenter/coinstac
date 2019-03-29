b81eab971c1679f0fb9969c461496d3c8320aaf6
Javier Romero <javiertomasromero@gmail.com>
2019-03-29 17:20:38 -0600

    Further refinements based on feedback/notes from Ross

--- packages/coinstac-ui/app/render/components/maps/maps-list.jsx
+++ packages/coinstac-ui/app/render/components/maps/maps-list.jsx
@@ -29,12 +29,8 @@ class MapsList extends Component {
     this.setState({ consortium });
   }
 
-  getMapped(member, owner, consortium) {
-    const { auth: { user } } = this.props;
-    const actions = [];
-    const text = [];
+  getMapped(consortium) {
     let isMapped = false;
-
     if (this.props.associatedConsortia.length > 0) {
       const assocCons = this.props.associatedConsortia.find(c => c.id === consortium.id);
       if (assocCons && assocCons.isMapped) {
@@ -56,11 +52,7 @@ class MapsList extends Component {
           itemObject={consortium}
           itemOptions
           itemMapped={
-            this.getMapped(
-              isUserA(user.id, consortium.members),
-              isUserA(user.id, consortium.owners),
-              consortium
-            )
+            this.getMapped(consortium)
           }
           pipelineId={pipeline.name}
           setConsortium={this.setConsortium}
@@ -80,16 +72,24 @@ class MapsList extends Component {
 
   render() {
     const {
+      auth: { user },
       consortia,
       pipelines,
     } = this.props;
 
+    const {
+      consortium,
+    } = this.state;
+
     return (
       <div>
-      {this.state.consortium ?
+      {consortium ?
         <MapsEdit
           consortia={consortia}
-          consortium={this.state.consortium}
+          consortium={consortium}
+          mapped={
+            this.getMapped(consortium)
+          }
           pipelines={this.props.pipelines}
           runs={this.props.runs}
         />:
