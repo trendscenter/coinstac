1c5f2e4d82c84b523345f60adf2b5e4a3f2cbc37
Ross Kelly <rkelly@mrn.org>
2019-06-30 17:51:00 -0600

    fix pipe not handling dupes, move reg to resample img

--- packages/coinstac-client-core/src/index.js
+++ packages/coinstac-client-core/src/index.js
@@ -223,6 +223,12 @@ class CoinstacClient {
           const pathsep = new RegExp(`${escape(path.sep)}|:`, 'g');
           linkPromises.push(
             linkAsync(filesArray[i], path.resolve(this.appDirectory, this.clientId, runId, filesArray[i].replace(pathsep, '-')))
+            .catch((e) => {
+              // permit dupes
+              if (e.code && e.code !== 'EEXIST') {
+                throw e;
+              }
+            })
           );
         }
 
