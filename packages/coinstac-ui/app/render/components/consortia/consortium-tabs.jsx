d256d987f39f12340d7e75fdbf1f06f0aab5935e
kalitopix <kalitopix@gmail.com>
2019-08-26 13:46:07 -0400

    show pipeline and run tab only for consortium update

--- packages/coinstac-ui/app/render/components/consortia/consortium-tabs.jsx
+++ packages/coinstac-ui/app/render/components/consortia/consortium-tabs.jsx
@@ -187,7 +187,9 @@ class ConsortiumTabs extends Component {
     const { user } = this.props.auth;
     const { selectedTabIndex, consortium, consortiumUsers } = this.state;
 
-    const title = this.state.consortium.id
+    const isEditingConsortium = !!this.state.consortium.id;
+
+    const title = isEditingConsortium
       ? 'Consortium Edit'
       : 'Consortium Creation';
 
@@ -207,8 +209,8 @@ class ConsortiumTabs extends Component {
           id="consortium-tabs"
         >
           <Tab label="About" />
-          <Tab label="Pipelines" />
-          { isOwner && <Tab label="Runs" /> }
+          { isEditingConsortium && <Tab label="Pipelines" /> }
+          { isEditingConsortium && isOwner && <Tab label="Runs" /> }
         </Tabs>
         {
           selectedTabIndex === 0
