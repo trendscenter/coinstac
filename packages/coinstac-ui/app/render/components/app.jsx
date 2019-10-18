3c32610a7be99dfcfd98e4d6da17f643f6012d8b
Eduardo Rocha <eduardorochacg@gmail.com>
2019-10-18 12:46:03 -0300

    Remove splash screen from separate window

--- packages/coinstac-ui/app/render/components/app.jsx
+++ packages/coinstac-ui/app/render/components/app.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { findDOMNode } from 'react-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { ipcRenderer } from 'electron';
@@ -26,18 +27,30 @@ class App extends Component { // eslint-disable-line react/prefer-stateless-func
     super(props);
 
     this.state = { checkJWT: false };
+
+    this.transitionSplashScreenIntoApp = this.transitionSplashScreenIntoApp.bind(this);
   }
 
   componentDidMount() {
-    this.props.autoLogin()
-    .then(() => {
-      this.setState({ checkJWT: true });
-    });
+    const {
+      autoLogin,
+      setError,
+      logout,
+      router,
+    } = this.props;
+
+    // Remove splash screen
+    setTimeout(this.transitionSplashScreenIntoApp, 1000);
+
+    autoLogin()
+      .then(() => {
+        this.setState({ checkJWT: true });
+      });
 
     ipcRenderer.on(EXPIRED_TOKEN, () => {
-      this.props.setError(EXPIRED_TOKEN);
-      this.props.logout();
-      this.props.router.push('/login');
+      setError(EXPIRED_TOKEN);
+      logout();
+      router.push('/login');
     });
   }
 
@@ -45,14 +58,27 @@ class App extends Component { // eslint-disable-line react/prefer-stateless-func
     ipcRenderer.removeAllListeners(EXPIRED_TOKEN);
   }
 
+  transitionSplashScreenIntoApp() {
+    const splashComponent = document.getElementById('splash-component');
+    splashComponent.className = 'fade-out';
+
+    // eslint-disable-next-line react/no-find-dom-node
+    const appComponent = findDOMNode(this);
+    appComponent.className = 'app fade-in';
+
+    setTimeout(() => splashComponent.parentNode.removeChild(splashComponent), 500);
+  }
+
   render() {
     const { children, loading: { isLoading }, notifications } = this.props;
+
+    const { checkJWT } = this.state;
 
     return (
       <div className="app">
         <ActivityIndicator visible={isLoading} />
 
-        {this.state.checkJWT && children}
+        { checkJWT && children }
 
         <Notifications
           notifications={notifications}
@@ -70,10 +96,13 @@ App.defaultProps = {
 };
 
 App.propTypes = {
+  router: PropTypes.object.isRequired,
   autoLogin: PropTypes.func.isRequired,
   children: PropTypes.node.isRequired,
   loading: PropTypes.object.isRequired,
   notifications: PropTypes.array,
+  setError: PropTypes.func.isRequired,
+  logout: PropTypes.func.isRequired,
 };
 
 function mapStateToProps({ loading, notifications }) {
