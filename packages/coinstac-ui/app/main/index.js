e5d97db4c6a746e10e97a43f40cb1462caa624bd
Eduardo Rocha <eduardorochacg@gmail.com>
2020-03-02 12:05:12 -0300

    Fix bug on start pipeline without data

--- packages/coinstac-ui/app/main/index.js
+++ packages/coinstac-ui/app/main/index.js
@@ -161,32 +161,8 @@ loadConfig()
         });
       });
     });
-    /**
-   * IPC Listener to start pipeline
-   * @param {Object} consortium
-   * @param {String} consortium.id The id of the consortium starting the pipeline
-   * @param {Object[]} consortium.pipelineSteps An array of the steps involved in
-   *  this pipeline run according to the consortium
-   * @param {String[]} dataMappings Mapping of pipeline variables into data file columns
-   * @param {Object} run
-   * @param {String} run.id The id of the current run
-   * @param {Object[]} run.pipelineSteps An array of the steps involved in this pipeline run
-   *  according to the run
-   * @return {Promise<String>} Status message
-   */
-    ipcMain.on('start-pipeline', (event, {
-      consortium, dataMappings, pipelineRun,
-    }) => {
-      const { filesArray, steps } = runPipelineFunctions.parsePipelineInput(pipelineRun.pipelineSnapshot, dataMappings);
 
-      const run = {
-        ...pipelineRun,
-        pipelineSnapshot: {
-          ...pipelineRun.pipelineSnapshot,
-          steps,
-        },
-      };
-
+    function startPipelineRun(run, filesArray, consortium) {
       const pipeline = run.pipelineSnapshot;
 
       mainWindow.webContents.send('save-local-run', { run });
@@ -258,13 +234,13 @@ loadConfig()
         .then(() => {
           logger.verbose('############ Client starting pipeline');
 
-          const pipelineName = pipeline.name
-          const consortiumName = consortium.name
+          const pipelineName = pipeline.name;
+          const consortiumName = consortium.name;
 
           ipcFunctions.sendNotification(
             'Pipeline started',
             `Pipeline ${pipelineName} started on consortia ${consortiumName}`
-          )
+          );
 
           return initializedCore.startPipeline(
             null,
@@ -287,7 +263,7 @@ loadConfig()
                 ipcFunctions.sendNotification(
                   'Pipeline finished',
                   `Pipeline ${pipelineName} finished on consortia ${consortiumName}`
-                )
+                );
 
                 return initializedCore.unlinkFiles(run.id)
                   .then(() => {
@@ -345,6 +321,37 @@ loadConfig()
               });
             });
         });
+    }
+
+    /**
+   * IPC Listener to start pipeline
+   * @param {Object} consortium Consortium starting the pipeline
+   * @param {Object} dataMappings Mapping of pipeline variables into data file columns
+   * @param {Object} pipelineRun Current run details
+   * @return {Promise<String>} Status message
+   */
+    ipcMain.on('start-pipeline', (event, {
+      consortium, dataMappings, pipelineRun,
+    }) => {
+      try {
+        const { filesArray, steps } = runPipelineFunctions.parsePipelineInput(
+          pipelineRun.pipelineSnapshot, dataMappings
+        );
+
+        const run = {
+          ...pipelineRun,
+          pipelineSnapshot: {
+            ...pipelineRun.pipelineSnapshot,
+            steps,
+          },
+        };
+
+        startPipelineRun(run, filesArray, consortium);
+      } catch (error) {
+        mainWindow.webContents.send('notify-warning', {
+          message: error.message,
+        });
+      }
     });
 
     /**
