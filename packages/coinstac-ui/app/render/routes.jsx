6f8662b7956049e0d9b77cf30d1887ed7931204f
kalitopix <kalitopix@gmail.com>
2020-03-02 13:08:41 -0500

    Added page for issue reporting to github

--- packages/coinstac-ui/app/render/routes.jsx
+++ packages/coinstac-ui/app/render/routes.jsx
@@ -23,6 +23,7 @@ import Pipeline from './components/pipelines/pipeline';
 import ResultsList from './components/results/results-list';
 import Result from './components/results/result';
 import Settings from './components/user/settings';
+import Issues from './components/issues';
 import Threads from './components/threads';
 import Logs from './components/logs-display/logs';
 
@@ -58,6 +59,7 @@ export default (
         <Route path="new" component={ComputationSubmission} />
       </Route>
       <Route path="settings" component={Settings} />
+      <Route path="issues" component={Issues} />
       <Route path="threads" component={Threads} />
       <Route path="logs" component={Logs} />
     </Route>
