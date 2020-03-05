16e203999b7171ae4d4945382c41c3cffdb1fdef
kalitopix <kalitopix@gmail.com>
2020-03-05 14:16:33 -0500

    Removed log

--- packages/coinstac-ui/app/render/state/ducks/docker.js
+++ packages/coinstac-ui/app/render/state/ducks/docker.js
@@ -38,8 +38,6 @@ export const removeImage = applyAsyncLoading((compId, imgName, imgId) => dispatc
   }));
 
 export const updateDockerOutput = (output => (dispatch) => {
-  console.log('------------------------')
-  console.log(output)
   if (output.output[0].status && output.output[0].status === 'error') {
     dispatch(notifyError({ message: `Docker Error with ${output.compName}` }));
   } else if (output.output[0].id && output.output[0].id.indexOf('-complete') > -1) {
