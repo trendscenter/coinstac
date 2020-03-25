57fd59a53c22dc2f6f0e9c651038df6f2a6f563e
Eduardo Rocha <eduardorochacg@gmail.com>
2020-03-25 11:15:00 -0300

    Fix state rehydrate on consecutive logins

--- packages/coinstac-ui/app/render/components/dashboard/dashboard.jsx
+++ packages/coinstac-ui/app/render/components/dashboard/dashboard.jsx
@@ -29,7 +29,7 @@ import {
   updateDockerOutput,
 } from '../../state/ducks/docker';
 import { updateUserConsortiaStatuses, updateUserPerms } from '../../state/ducks/auth';
-import { appendLogMessage, clearLogs } from '../../state/ducks/app';
+import { appendLogMessage } from '../../state/ducks/app';
 import {
   COMPUTATION_CHANGED_SUBSCRIPTION,
   CONSORTIUM_CHANGED_SUBSCRIPTION,
@@ -179,9 +179,6 @@ class Dashboard extends Component {
 
       this.props.updateLocalRun(arg.run.id, { error: arg.run.error, status: 'error' });
     });
-
-    const { clearLogs } = this.props;
-    clearLogs();
 
     ipcRenderer.on('log-message', (event, arg) => {
       const { appendLogMessage } = this.props;
@@ -565,7 +562,6 @@ Dashboard.propTypes = {
   updateDockerOutput: PropTypes.func.isRequired,
   updateLocalRun: PropTypes.func.isRequired,
   updateUserConsortiumStatus: PropTypes.func.isRequired,
-  clearLogs: PropTypes.func.isRequired,
   appendLogMessage: PropTypes.func.isRequired,
   writeLog: PropTypes.func.isRequired,
   updateUserPerms: PropTypes.func.isRequired,
@@ -668,7 +664,6 @@ const connectedComponent = connect(mapStateToProps,
     updateUserConsortiaStatuses,
     writeLog,
     updateUserPerms,
-    clearLogs,
     appendLogMessage,
   })(DashboardWithData);
 
