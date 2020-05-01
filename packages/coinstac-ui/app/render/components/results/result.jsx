5190d1b1268862d44e816550356b4c5c73a8d7f3
Ross Kelly <5331533+rssk@users.noreply.github.com>
2020-05-01 13:32:08 -0600

    more path changes

--- packages/coinstac-ui/app/render/components/results/result.jsx
+++ packages/coinstac-ui/app/render/components/results/result.jsx
@@ -273,7 +273,7 @@ class Result extends Component {
                 selectedDisplayType.type === 'images'
                 && (
                   <Images
-                    imagePath={`${appDirectory}/output/${user.id}/${run.id}/${plotData.file_name}`}
+                    imagePath={path.join(appDirectory, 'output', user.id, run.id, plotData.file_name)}
                     plotData={plotData}
                     title={`${consortium.name}_${run.pipelineSnapshot.name}`}
                   />
