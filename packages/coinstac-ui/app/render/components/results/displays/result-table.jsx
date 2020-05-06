0a14dcbf230257cc6db61f75bccb7bcb984b220e
pixelsaurus <javiertomasromero@gmail.com>
2020-05-06 13:22:42 -0600

    Fixing breaking render issue for MSR Freesurfer results

--- packages/coinstac-ui/app/render/components/results/displays/result-table.jsx
+++ packages/coinstac-ui/app/render/components/results/displays/result-table.jsx
@@ -178,7 +178,7 @@ class TableResult extends Component {
 
       let labels = [];
 
-      if (heading.includes('Global') && Array.isArray(data.covariate_labels[0])) {
+      if (heading.includes('Global') && data.covariate_labels && Array.isArray(data.covariate_labels[0])) {
         labels = data.covariate_labels[0]; // eslint-disable-line prefer-destructuring
       } else {
         labels = data.covariate_labels;
