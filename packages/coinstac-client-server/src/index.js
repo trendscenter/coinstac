ec3917b1ea236bc8648093add2df6cd775eb41fb
kalitopix <kalitopix@gmail.com>
2020-06-25 05:51:13 -0400

    Added pull and remove docker images to the client server

--- packages/coinstac-client-server/src/index.js
+++ packages/coinstac-client-server/src/index.js
@@ -3,7 +3,6 @@ const hapi = require('hapi');
 process.LOGLEVEL = 'silly';
 
 const config = require('./config');
-const routes = require('./routes');
 
 const server = new hapi.Server();
 server.connection({
@@ -11,8 +10,10 @@ server.connection({
   port: config.hapiPort,
 });
 
-server.route(routes);
-server.start((startErr) => {
-  if (startErr) throw startErr;
-  console.log(`Server running at: ${server.info.uri}`); // eslint-disable-line no-console
+require('./routes').then((routes) => {
+  server.route(routes);
+  server.start((startErr) => {
+    if (startErr) throw startErr;
+    console.log(`Server running at: ${server.info.uri}`); // eslint-disable-line no-console
+  });
 });
--- packages/coinstac-client-server/src/routes/pipeline.js
+++ packages/coinstac-client-server/src/routes/pipeline.js
@@ -1,13 +1,49 @@
 /* eslint-disable max-len */
 
-module.exports = [
-  {
-    method: 'GET',
-    path: '/',
-    config: {
-      handler: (_, res) => {
-        res({}).code(200);
+const PipelineManager = require('coinstac-pipeline');
+const { pullImagesFromList, removeImagesFromList } = require('coinstac-docker-manager');
+const path = require('path');
+const config = require('../config');
+
+const manager = PipelineManager.create({
+  mode: 'remote',
+  clientId: 'remote',
+  operatingDirectory: path.resolve(config.operatingDirectory, 'coinstac'),
+  remotePort: 3400,
+  mqttRemotePort: config.mqttServer.port,
+  mqttRemoteProtocol: config.mqttServer.protocol,
+  mqttRemoteURL: config.mqttServer.hostname,
+});
+
+module.exports = manager.then(() => {
+  return [
+    {
+      method: 'POST',
+      path: '/pullImages',
+      config: {
+        handler: async (req, res) => {
+          const { payload: { images } } = req;
+
+          pullImagesFromList(images)
+            .then(() => {
+              res('Pulled images successfully').code(200);
+            });
+        },
       },
     },
-  },
-];
+    {
+      method: 'POST',
+      path: '/removeImages',
+      config: {
+        handler: async (req, res) => {
+          const { payload: { images } } = req;
+
+          removeImagesFromList(images)
+            .then(() => {
+              res('Removed images successfully').code(200);
+            });
+        },
+      },
+    },
+  ];
+});
