c9704c5fd3f2bbe5786505a24b47c4c2eb5de8fc
Eduardo Rocha <eduardorochacg@gmail.com>
2020-07-22 15:43:18 -0300

    Test windows packaging and add mac config

--- packages/coinstac-ui/scripts/pre-build.js
+++ packages/coinstac-ui/scripts/pre-build.js
@@ -1,7 +1,5 @@
 const fs = require('fs');
 
-console.log('AA', process.env.NODE_ENV);
-
 if (process.env.NODE_ENV === 'development') {
   fs.copyFileSync('./config/local-development.json', './config/local.json');
 } else if (process.env.NODE_ENV === 'production') {
