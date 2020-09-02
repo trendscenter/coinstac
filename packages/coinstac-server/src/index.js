33a080c51ea0dc0324796fdf66a398f31b6875e6
Ross Kelly <rkelly@mrn.org>
2020-09-02 13:42:50 -0600

    add ci scripts and docker images

--- packages/coinstac-server/src/index.js
+++ packages/coinstac-server/src/index.js
@@ -1,10 +1,18 @@
+/* eslint-disable no-console */
 const hapi = require('hapi');
 const axios = require('axios');
 // set w/ config etc post release
 process.LOGLEVEL = 'silly';
 const { pullImages } = require('coinstac-docker-manager');
 const graphqlSchema = require('coinstac-graphql-schema');
+const { program } = require('commander');
 const routes = require('./routes');
+
+program.version(require('../package.json').version);
+
+program
+  .option('-n, --no-images', 'do not download computation images');
+program.parse(process.argv);
 
 let idToken = '';
 
@@ -18,42 +26,46 @@ const apiServer = `http://${process.env.API_SERVER_HOSTNAME}:${process.env.API_S
  * On server start, pull in comps from DB whitelist and download via docker
  * @return {Promise<string>} Success flag
  */
-axios.post(
-  `${apiServer}/authenticate`,
-	{
-		username: process.env.SERVER_API_USERNAME,
-		password: process.env.SERVER_API_PASSWORD,
-	}
-)
-  .then((token) => {
-    idToken = token.data.id_token;
-    axios.defaults.headers.common.Authorization = `Bearer ${idToken}`;
-  })
-  .then(() => axios.get(`${apiServer}/graphql?query=${graphqlSchema.queries.allDockerImages}`))
-  .then(({ data: { data: { fetchAllComputations } } }) => {
-    const comps = fetchAllComputations.map(comp => ({
-      img: comp.computation.dockerImage,
-      compId: comp.id,
-      compName: comp.meta.name,
-    }));
-    return pullImages(comps);
-  })
-  .then((pullStreams) => {
-    console.log('Updating docker images'); // eslint-disable-line no-console
-    pullStreams.forEach((obj) => {
-      if (typeof obj.stream.pipe === 'function') {
-        obj.stream.pipe(process.stdout);
-      } else {
-        process.stdout.write('.');
-      }
-    });
-  })
-  .catch(console.log) // eslint-disable-line no-console
-  .then(() => routes)
+let startup = Promise.resolve();
+if (program.noImages) {
+  startup = axios.post(
+    `${apiServer}/authenticate`,
+    {
+      username: process.env.SERVER_API_USERNAME,
+      password: process.env.SERVER_API_PASSWORD,
+    }
+  )
+    .then((token) => {
+      idToken = token.data.id_token;
+      axios.defaults.headers.common.Authorization = `Bearer ${idToken}`;
+    })
+    .then(() => axios.get(`${apiServer}/graphql?query=${graphqlSchema.queries.allDockerImages}`))
+    .then(({ data: { data: { fetchAllComputations } } }) => {
+      const comps = fetchAllComputations.map(comp => ({
+        img: comp.computation.dockerImage,
+        compId: comp.id,
+        compName: comp.meta.name,
+      }));
+      return pullImages(comps);
+    })
+    .then((pullStreams) => {
+      console.log('Updating docker images');
+      pullStreams.forEach((obj) => {
+        if (typeof obj.stream.pipe === 'function') {
+          obj.stream.pipe(process.stdout);
+        } else {
+          process.stdout.write('.');
+        }
+      });
+    })
+    .catch(console.log);
+}
+
+startup.then(() => routes)
   .then((routes) => {
     server.route(routes);
     server.start((startErr) => {
       if (startErr) throw startErr;
-      console.log(`Server running at: ${server.info.uri}`); // eslint-disable-line no-console
+      console.log(`Server running at: ${server.info.uri}`);
     });
   });
--- scripts/aws-secret-fetch.sh
+++ scripts/aws-secret-fetch.sh
@@ -6,21 +6,25 @@ if [ $1 -eq 0 ] || [ $2 = "" ] || [ $3 = "" ]; then
     echo ""
     echo "Second argument is the config env [prod, dev, local]"
     echo ""
-    echo "Third is the output type [export, systemd, json]"
+    echo "Third is the output type [export, systemd, json, shell]"
     exit 1
 fi
-unset AWS_ACCESS_KEY_ID
-unset AWS_SECRET_ACCESS_KEY
-unset AWS_SESSION_TOKEN
 
-SESSION=$(aws sts get-session-token --serial-number $MFA_ARN --token-code $1)
-ACCESSKEYID=$(echo $SESSION | jq -r .Credentials.AccessKeyId)
-SECRETACCESSKEY=$(echo $SESSION | jq -r .Credentials.SecretAccessKey)
-SESSIONTOKEN=$(echo $SESSION | jq -r .Credentials.SessionToken)
+if [ $4 != "ci" ]
+  unset AWS_ACCESS_KEY_ID
+  unset AWS_SECRET_ACCESS_KEY
+  unset AWS_SESSION_TOKEN
 
-export AWS_ACCESS_KEY_ID=$ACCESSKEYID
-export AWS_SECRET_ACCESS_KEY=$SECRETACCESSKEY
-export AWS_SESSION_TOKEN=$SESSIONTOKEN
+  SESSION=$(aws sts get-session-token --serial-number $MFA_ARN --token-code $1)
+  ACCESSKEYID=$(echo $SESSION | jq -r .Credentials.AccessKeyId)
+  SECRETACCESSKEY=$(echo $SESSION | jq -r .Credentials.SecretAccessKey)
+  SESSIONTOKEN=$(echo $SESSION | jq -r .Credentials.SessionToken)
+
+  export AWS_ACCESS_KEY_ID=$ACCESSKEYID
+  export AWS_SECRET_ACCESS_KEY=$SECRETACCESSKEY
+  export AWS_SESSION_TOKEN=$SESSIONTOKEN
+fi
+
 if [ $2 != prod ]; then
   SECRETID="coinstac-$2"
 else
@@ -37,6 +41,8 @@ if [ $3 = export ]; then
   echo $CONFIG | jq -r 'to_entries[] | "export \(.key)=\(.value)"'
 elif [ $3 = systemd ]; then
   echo $CONFIG | jq -r 'to_entries[] | "\(.key)=\(.value)"'
+elif [ $3 = shell ]; then
+  export $CONFIG | jq -r 'to_entries[] | "\(.key)=\(.value)"'
 else
   echo $CONFIG
 fi
