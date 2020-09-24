389cc6c16836348f3a01d4bc804ed74480ee605f
Ross Kelly <rkelly@mrn.org>
2020-09-24 15:46:11 -0600

    add the darn script

---
+++ scripts/dockerbuild-for-ci.sh
@@ -0,0 +1,4 @@
+docker build -f packages/coinstac-images/coinstac/dockerfiles/Dockerfile-ci -t coinstacteam/coinstac:ci .
+docker build -f packages/coinstac-images/coinstac/dockerfiles/Dockerfile-ci-ui -t coinstacteam/coinstac-ui:ci .
+docker build -f packages/coinstac-images/coinstac/dockerfiles/Dockerfile-api -t coinstacteam/coinstac-api . 
+docker build -f packages/coinstac-images/coinstac/dockerfiles/Dockerfile-server -t coinstacteam/coinstac-server .
