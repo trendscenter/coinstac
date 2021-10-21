19d41eaf6e87a4dc98b15c327d77ffac65cbfdb9
Eduardo Rocha <eduardorochacg@gmail.com>
2021-10-21 15:11:55 -0300

    Fix fieldType for files

--- packages/coinstac-ui/app/render/components/maps/fields/maps-files-field.jsx
+++ packages/coinstac-ui/app/render/components/maps/fields/maps-files-field.jsx
@@ -30,7 +30,7 @@ function MapsFilesField({
   fieldName, fieldDataMap, fieldDescription, onChange, classes,
 }) {
   function setSelectedFiles(selectedFiles) {
-    onChange(fieldName, { type: fieldDescription.type, files: selectedFiles });
+    onChange(fieldName, { fieldType: fieldDescription.type, files: selectedFiles });
   }
 
   function appendSelectedFiles(selectedFiles) {
