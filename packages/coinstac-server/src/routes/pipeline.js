6f3662d4944dcaeb6ff4927d1e5e636f4569feee
dylanmartin <dylanrossmartin@gmail.com>
2021-10-26 15:31:08 -0400

    added try/catch around startPipeline

--- packages/coinstac-server/src/routes/pipeline.js
+++ packages/coinstac-server/src/routes/pipeline.js
@@ -111,29 +111,33 @@ module.exports = manager.then((remotePipelineManager) => {
               pullImagesFromList(computationImageList)
                 .then(() => pruneImages())
                 .then(() => {
-                  const { result, stateEmitter } = remotePipelineManager.startPipeline({
-                    clients: run.clients,
-                    spec: run.pipelineSnapshot,
-                    runId: run.id,
-                    timeout: run.pipelineSnapshot.timeout,
-                  });
-                  res({}).code(201);
-
-                  stateEmitter.on('update', (data) => {
-                    // TODO:  console most likely should be removed post proto development
-                    // or made less noisy
-                    updateRunMessageQueue.push({ runId: run.id, data });
-                    updateRunState();
-                  });
-
-                  return result
-                    .then((result) => {
-                      saveResults(run.id, result);
-                    })
-                    .catch((error) => {
-                      console.log(error); // eslint-disable-line no-console
-                      saveError(run.id, error);
+                  try {
+                    const { result, stateEmitter } = remotePipelineManager.startPipeline({
+                      clients: run.clients,
+                      spec: run.pipelineSnapshot,
+                      runId: run.id,
+                      timeout: run.pipelineSnapshot.timeout,
                     });
+                    res({}).code(201);
+
+                    stateEmitter.on('update', (data) => {
+                      // TODO:  console most likely should be removed post proto development
+                      // or made less noisy
+                      updateRunMessageQueue.push({ runId: run.id, data });
+                      updateRunState();
+                    });
+
+                    return result
+                      .then((result) => {
+                        saveResults(run.id, result);
+                      })
+                      .catch((error) => {
+                        console.log(error); // eslint-disable-line no-console
+                        saveError(run.id, error);
+                      });
+                  } catch (error) {
+                    res({ error }).code(500);
+                  }
                 });
             });
         },
