7b094a23784bae156ff9405aa665cdd58645a1e5
Eduardo Rocha <eduardorochacg@gmail.com>
2021-10-28 16:05:32 -0300

    Remove notistack library

---
+++ packages/coinstac-ui/app/render/components/notification/index.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import Snackbar from '@material-ui/core/Snackbar';
+import Alert from '@material-ui/lab/Alert';
+
+import useStyles from './notification.styles';
+
+function Notification({
+  duration, id, message, severity, index, onClose,
+}) {
+  const classes = useStyles({ index });
+
+  const [visible, setVisible] = useState(true);
+
+  function handleClose() {
+    setVisible(false);
+    onClose(id);
+  }
+
+  return (
+    <Snackbar
+      open={visible}
+      onClose={handleClose}
+      autoHideDuration={duration}
+      anchorOrigin={{
+        vertical: 'bottom',
+        horizontal: 'left',
+      }}
+      className={classes.root}
+    >
+      <Alert severity={severity}>
+        { message }
+      </Alert>
+    </Snackbar>
+  );
+}
+
+Notification.propTypes = {
+  duration: PropTypes.number.isRequired,
+  id: PropTypes.string.isRequired,
+  message: PropTypes.string.isRequired,
+  severity: PropTypes.string.isRequired,
+  index: PropTypes.number.isRequired,
+  onClose: PropTypes.func.isRequired,
+};
+
+export default Notification;
