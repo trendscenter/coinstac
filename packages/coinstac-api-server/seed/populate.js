9a592400b07846581ed006336da6b881d3cb08dd
Ross Kelly <5331533+rssk@users.noreply.github.com>
2021-12-14 14:14:36 -0500

    return promise from db call

--- packages/coinstac-api-server/seed/populate.js
+++ packages/coinstac-api-server/seed/populate.js
@@ -252,7 +252,7 @@ async function populatePipelines() {
 async function populateRuns() {
   const db = database.getDbInstance();
 
-  db.collection('runs').insertMany([
+  return db.collection('runs').insertMany([
     {
       _id: RUN_IDS[0],
       clients: {
