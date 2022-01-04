43b99184f794cd5526b365995e658737c5d3a7a1
Eduardo Rocha <eduardorochacg@gmail.com>
2022-01-04 16:30:14 -0300

    Update comment on the component responsible for starting decentralized runs

--- packages/coinstac-ui/app/render/components/runs/effects/useStartDecentralizedRun.jsx
+++ packages/coinstac-ui/app/render/components/runs/effects/useStartDecentralizedRun.jsx
@@ -1,4 +1,4 @@
-import { useRef, useEffect } from 'react';
+import { useEffect } from 'react';
 import { useQuery, useSubscription } from '@apollo/client';
 import { get } from 'lodash';
 import { useSelector, useDispatch } from 'react-redux';
@@ -6,8 +6,11 @@ import { useSelector, useDispatch } from 'react-redux';
 import { FETCH_ALL_CONSORTIA_QUERY, RUN_STARTED_SUBSCRIPTION } from '../../../state/graphql/functions';
 import { startRun } from '../../../state/ducks/runs';
 
-/*
- * This effect checks if there are runs that must be started locally when the app first starts.
+/**
+ * This effect starts decentralized runs based on the RUN_STARTED_SUBSCRIPTION.
+ *
+ * The RUN_STARTED_SUBSCRIPTION has new data when a new decentralized run (of which the
+ * current user is a part of) is started.
  */
 function useStartDecentralizedRun() {
   const dispatch = useDispatch();
