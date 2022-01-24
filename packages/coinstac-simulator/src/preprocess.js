c3ce69c903e05bfeb1474035fca881fe8b32fb22
Dylan Martin <dylanrossmartin@gmail.com>
2022-01-24 12:57:05 -0500

    massage inputspec

--- packages/coinstac-simulator/src/preprocess.js
+++ packages/coinstac-simulator/src/preprocess.js
@@ -1,5 +1,7 @@
 /* eslint-disable camelcase */
 const axios = require('axios');
+const fs = require('fs');
+const { parse } = require('csv-parse/sync');
 const config = require('./config');
 
 axios.defaults.baseURL = `${config.protocol}://${config.apiServer}:${config.port}`;
@@ -34,5 +36,101 @@ async function fetchPreprocessComputations(username, password) {
   return computations;
 }
 
+const brainRegionKeys = [
+  '3rd-Ventricle',
+  '4th-Ventricle',
+  '5th-Ventricle',
+  'Brain-Stem',
+  'BrainSegVol',
+  'BrainSegVol-to-eTIV',
+  'BrainSegVolNotVent',
+  'BrainSegVolNotVentSurf',
+  'CC_Anterior',
+  'CC_Central',
+  'CC_Mid_Anterior',
+  'CC_Mid_Posterior',
+  'CC_Posterior',
+  'CortexVol',
+  'CorticalWhiteMatterVol',
+  'CSF',
+  'EstimatedTotalIntraCranialVol',
+  'Left-Accumbens-area',
+  'Left-Amygdala',
+  'Left-Caudate',
+  'Left-Cerebellum-Cortex',
+  'Left-Cerebellum-White-Matter',
+  'Left-choroid-plexus',
+  'Left-Hippocampus',
+  'Left-Inf-Lat-Vent',
+  'Left-Lateral-Ventricle',
+  'Left-non-WM-hypointensities',
+  'Left-Pallidum',
+  'Left-Putamen',
+  'Left-Thalamus-Proper',
+  'Left-VentralDC',
+  'Left-vessel',
+  'Left-WM-hypointensities',
+  'lhCortexVol',
+  'lhCorticalWhiteMatterVol',
+  'lhSurfaceHoles',
+  'MaskVol',
+  'MaskVol-to-eTIV',
+  'non-WM-hypointensities',
+  'Optic-Chiasm',
+  'rhCortexVol',
+  'rhCorticalWhiteMatterVol',
+  'rhSurfaceHoles',
+  'Right-Accumbens-area',
+  'Right-Amygdala',
+  'Right-Caudate',
+  'Right-Cerebellum-Cortex',
+  'Right-Cerebellum-White-Matter',
+  'Right-choroid-plexus',
+  'Right-Hippocampus',
+  'Right-Inf-Lat-Vent',
+  'Right-Lateral-Ventricle',
+  'Right-non-WM-hypointensities',
+  'Right-Pallidum',
+  'Right-Putamen',
+  'Right-Thalamus-Proper',
+  'Right-VentralDC',
+  'Right-vessel',
+  'Right-WM-hypointensities',
+  'SubCortGrayVol',
+  'SupraTentorialVol',
+  'SupraTentorialVolNotVent',
+  'SupraTentorialVolNotVentVox',
+  'SurfaceHoles',
+  'TotalGrayVol',
+  'WM-hypointensities',
+];
 
-module.exports = { fetchPreprocessComputations };
+
+function createInputSpec(baseInputSpec, csvPath) {
+  const inputSpec = baseInputSpec;
+  Object.keys(inputSpec).forEach((key) => {
+    if (inputSpec[key].source === 'owner') {
+      inputSpec[key] = { value: inputSpec[key].default };
+    }
+  });
+  inputSpec.data = {
+    value: [{
+      type: 'NiFTI', value: brainRegionKeys,
+    }],
+  };
+  inputSpec.covariates = { value: {} };
+  const records = parse(fs.readFileSync(csvPath));
+  const headers = records[0];
+  for (let i = 1; i < records.length; i++) {
+    const record = records[i];
+    const covariate = {};
+    for (let j = 1; j < headers.length; j++) {
+      covariate[headers[j]] = record[j];
+    }
+    inputSpec.covariates.value[records[i][0]] = covariate;
+  }
+  return inputSpec;
+}
+
+module.exports = { fetchPreprocessComputations, createInputSpec };
+
