986b01ce8251ec2b08f09440f550df119981713f
Dylan Martin <dylanrossmartin@gmail.com>
2022-02-08 10:22:44 -0500

    fix side effects and clearer verbiage

--- packages/coinstac-simulator/src/preprocess.js
+++ packages/coinstac-simulator/src/preprocess.js
@@ -125,19 +125,19 @@ const brainRegionKeys = [
 ];
 
 
-function createInputSpec(baseInputSpec, csvPath) {
-  const inputSpec = baseInputSpec;
-  Object.keys(inputSpec).forEach((key) => {
-    if (inputSpec[key].source === 'owner') {
-      inputSpec[key] = { value: inputSpec[key].default };
+function createInputMap(baseInputSpec, csvPath) {
+  const inputMap = JSON.parse(JSON.stringify(baseInputSpec));
+  Object.keys(inputMap).forEach((key) => {
+    if (inputMap[key].source === 'owner') {
+      inputMap[key] = { value: inputMap[key].default };
     }
   });
-  inputSpec.data = {
+  inputMap.data = {
     value: [{
       type: 'NiFTI', value: brainRegionKeys,
     }],
   };
-  inputSpec.covariates = { value: {} };
+  inputMap.covariates = { value: {} };
   const records = parse(fs.readFileSync(csvPath));
   const headers = records[0];
   for (let i = 1; i < records.length; i += 1) {
@@ -146,9 +146,9 @@ function createInputSpec(baseInputSpec, csvPath) {
     for (let j = 1; j < headers.length; j += 1) {
       covariate[headers[j]] = record[j];
     }
-    inputSpec.covariates.value[records[i][0]] = covariate;
+    inputMap.covariates.value[records[i][0]] = covariate;
   }
-  return inputSpec;
+  return inputMap;
 }
 
 async function prepareDirectory(pipelineSpec, baseDir) {
@@ -158,4 +158,4 @@ async function prepareDirectory(pipelineSpec, baseDir) {
   }));
 }
 
-module.exports = { fetchPreprocessComputations, createInputSpec, prepareDirectory };
+module.exports = { fetchPreprocessComputations, createInputMap, prepareDirectory };
