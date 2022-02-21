bd347f919990b16c132f07a9c03c9f9ba51e0bf3
Dylan Martin <dylanrossmartin@gmail.com>
2022-02-21 15:48:40 -0500

    add logic to update compspec

--- packages/coinstac-ui/app/render/components/computations/computation-io.jsx
+++ packages/coinstac-ui/app/render/components/computations/computation-io.jsx
@@ -1,13 +1,12 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { graphql } from '@apollo/react-hoc';
+import { graphql, useMutation } from '@apollo/react-hoc';
 import ReactJson from 'react-json-view';
+import { Button, TextareaAutosize } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
 import { FETCH_COMPUTATION_QUERY } from '../../state/graphql/functions';
+import { ADD_COMPUTATION_MUTATION } from '../../state/graphql/functions';
 import { compIOProp } from '../../state/graphql/props';
-import { Button, TextareaAutosize } from '@material-ui/core';
-import { useState } from 'react';
-import { useEffect } from 'react';
 
 const styles = theme => ({
   wrapper: {
@@ -19,21 +18,44 @@ const ComputationIO = ({ compIO, classes }) => {
   if (!compIO) {
     return null;
   }
-  const inputSpec = JSON.stringify(compIO.computation.input, null, 4)
+
+  // get the complete compspec
+  // display the complete compspec
+  const compSpecDbObject = JSON.parse(JSON.stringify(compIO));
+  const inputSpec = JSON.stringify(compSpecDbObject.computation.input, null, 4);
   const [textAreaValue, setTextAreaValue] = useState(inputSpec);
   const [isModified, setIsModified] = useState(false);
-  
-  useEffect(()=>{
-    if(!inputSpec.includes(textAreaValue)){
-      setIsModified(true)
+
+  const [addComputation, { data, loading, error }] = useMutation(ADD_COMPUTATION_MUTATION);
+
+  useEffect(() => {
+    if (!inputSpec.includes(textAreaValue)) {
+      setIsModified(true);
     }
-  }, [textAreaValue])
+  }, [textAreaValue]);
+
+  const updateComputation = () => {
+    // merge what has been changed into the complete compspec
+    compSpecDbObject.computation.input = JSON.parse(textAreaValue);
+    compSpecDbObject.meta = { id: compSpecDbObject.id };
+    addComputation({ variables: { computationSchema: compSpecDbObject } });
+  };
 
 
   return (
     <div className={classes.wrapper}>
-      <TextareaAutosize value={textAreaValue} onChange={e=>{setTextAreaValue(e.target.value)}} style={{width: '100%'}}></TextareaAutosize>
-    <Button disabled={!isModified}>update</Button>
+      <TextareaAutosize
+        value={textAreaValue}
+        onChange={(e) => { setTextAreaValue(e.target.value); }}
+        style={{ width: '100%' }}
+      />
+
+      <Button
+        disabled={!isModified}
+        onClick={updateComputation}
+      >
+        update
+      </Button>
     </div>
   );
 };
