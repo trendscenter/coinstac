7447b98b9787d24beca545257433d0ca6530d444
dylanmartin <dylanrossmartin@gmail.com>
2022-02-21 11:23:07 -0500

    comspec as textarea

--- packages/coinstac-ui/app/render/components/computations/computation-io.jsx
+++ packages/coinstac-ui/app/render/components/computations/computation-io.jsx
@@ -5,6 +5,9 @@ import ReactJson from 'react-json-view';
 import { withStyles } from '@material-ui/core/styles';
 import { FETCH_COMPUTATION_QUERY } from '../../state/graphql/functions';
 import { compIOProp } from '../../state/graphql/props';
+import { Button, TextareaAutosize } from '@material-ui/core';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 const styles = theme => ({
   wrapper: {
@@ -16,16 +19,21 @@ const ComputationIO = ({ compIO, classes }) => {
   if (!compIO) {
     return null;
   }
+  const inputSpec = JSON.stringify(compIO.computation.input, null, 4)
+  const [textAreaValue, setTextAreaValue] = useState(inputSpec);
+  const [isModified, setIsModified] = useState(false);
+  
+  useEffect(()=>{
+    if(!inputSpec.includes(textAreaValue)){
+      setIsModified(true)
+    }
+  }, [textAreaValue])
+
 
   return (
     <div className={classes.wrapper}>
-      <ReactJson
-        src={compIO}
-        theme="monokai"
-        displayDataTypes={false}
-        displayObjectSize={false}
-        enableClipboard={false}
-      />
+      <TextareaAutosize value={textAreaValue} onChange={e=>{setTextAreaValue(e.target.value)}} style={{width: '100%'}}></TextareaAutosize>
+    <Button disabled={!isModified}>update</Button>
     </div>
   );
 };
