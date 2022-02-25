e8575afba7af2c21f3406667f4f2b8e1d71d75fd
Dylan Martin <dylanrossmartin@gmail.com>
2022-02-25 14:36:09 -0500

    update works and notifies

--- packages/coinstac-ui/app/render/components/computations/computation-io.jsx
+++ packages/coinstac-ui/app/render/components/computations/computation-io.jsx
@@ -6,6 +6,8 @@ import { withStyles } from '@material-ui/core/styles';
 import { Button, TextareaAutosize } from '@material-ui/core';
 import { ADD_COMPUTATION_MUTATION, FETCH_COMPUTATION_QUERY } from '../../state/graphql/functions';
 import { compIOProp } from '../../state/graphql/props';
+import { notifySuccess, notifyError } from '../../state/ducks/notifyAndLog';
+import { connect } from 'react-redux';
 
 const styles = theme => ({
   wrapper: {
@@ -13,49 +15,87 @@ const styles = theme => ({
   },
 });
 
-const ComputationIO = ({ compIO, classes }) => {
+const ComputationIO = ({ compIO, classes, notifySuccess, notifyError }) => {
   if (!compIO) {
     return null;
   }
 
-  const compSpecDbObject = JSON.parse(JSON.stringify(compIO));
-  delete compSpecDbObject.id;
-  delete compSpecDbObject.__typename;
-  delete compSpecDbObject.computation.__typename;
-  delete compSpecDbObject.computation.remote.__typename;
-  delete compSpecDbObject.meta.__typename;
-  const inputSpec = JSON.stringify(compSpecDbObject, null, 4);
-  const [textAreaValue, setTextAreaValue] = useState(inputSpec);
-  const [isModified, setIsModified] = useState(false);
+  const prepareJSON = (info) => {
+    const compSpecDbObject = JSON.parse(JSON.stringify(info));
+    try {
+      delete compSpecDbObject.id;
+      delete compSpecDbObject.__typename;
+      delete compSpecDbObject.computation.__typename;
+      delete compSpecDbObject.computation.remote.__typename;
+      delete compSpecDbObject.meta.__typename;
+    } catch { }
+    return JSON.stringify(compSpecDbObject, null, 4);
+  };
+
+  const [textAreaValue, setTextAreaValue] = useState('');
+  const [isEditing, setIsEditing] = useState(false);
   const [addComputation] = useMutation(ADD_COMPUTATION_MUTATION);
 
-  const updateComputation = () => {
-    const val = JSON.parse(textAreaValue);
 
-    addComputation({ variables: { computationSchema: val } });
+  const updateComputation = () => {
+    try {
+      const val = JSON.parse(textAreaValue);
+      addComputation({
+        variables: { computationSchema: val },
+      }).then((e) => {
+        notifySuccess('Compspec updated');
+        setIsEditing(false);
+      }).catch((e) => {
+        notifyError(e.message);
+      });
+    } catch (e) {
+      notifyError(e.message);
+    }
   };
 
   useEffect(() => {
-    if (!inputSpec.includes(textAreaValue)) {
-      setIsModified(true);
-    }
-  }, [textAreaValue]);
+    setTextAreaValue(prepareJSON(compIO));
+  }, []);
+
 
   return (
-    <div>
-      <TextareaAutosize
-        value={textAreaValue}
-        onChange={(e) => { setTextAreaValue(e.target.value); }}
-        style={{ width: '100%' }}
-      />
-
+    <div className={classes.wrapper}>
+      {isEditing
+        ? (
+          <TextareaAutosize
+            value={textAreaValue}
+            onChange={(e) => { setTextAreaValue(e.target.value); }}
+            style={{ width: '100%' }}
+          />
+        )
+        : (
+          <ReactJson
+            src={JSON.parse(prepareJSON(compIO))}
+            theme="monokai"
+            displayDataTypes={false}
+            displayObjectSize={false}
+            enableClipboard={false}
+          />
+        )
+      }
       <Button
         color="primary"
-        disabled={!isModified}
-        onClick={updateComputation}
+        onClick={() => {
+          setIsEditing(!isEditing);
+        }}
       >
-        update
+        {isEditing ? 'cancel' : 'edit'}
       </Button>
+      {isEditing
+        && (
+          <Button
+            color="primary"
+            onClick={updateComputation}
+          >
+            update
+          </Button>
+        )
+      }
     </div>
   );
 };
@@ -67,8 +107,11 @@ ComputationIO.defaultProps = {
 ComputationIO.propTypes = {
   compIO: PropTypes.object,
   classes: PropTypes.object.isRequired,
+  notifyError: PropTypes.func.isRequired,
+  notifySuccess: PropTypes.func.isRequired,
 };
 
 const ComputationIOWithData = graphql(FETCH_COMPUTATION_QUERY, compIOProp)(ComputationIO);
+const ComputationIOWithAlert = connect(null, { notifySuccess, notifyError })(ComputationIOWithData);
 
-export default withStyles(styles)(ComputationIOWithData);
+export default withStyles(styles)(ComputationIOWithAlert);
