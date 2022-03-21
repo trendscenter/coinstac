90d6b6e3b3e72b32de83b2426506f8708c206e70
Ross Kelly <5331533+rssk@users.noreply.github.com>
2022-03-21 12:56:48 -0600

    Update auto-update-listener.jsx

--- packages/coinstac-ui/app/render/components/auto-update-listener.jsx
+++ packages/coinstac-ui/app/render/components/auto-update-listener.jsx
@@ -3,6 +3,9 @@ import { ipcRenderer } from 'electron';
 
 function AutoUpdateListener() {
   useEffect(() => {
+    ipcRenderer.on('auto-update-log', (event, message) => {
+      console.log(message); // eslint-disable-line no-console
+    });
     return () => {
       ipcRenderer.removeAllListeners('auto-update-log');
     };
