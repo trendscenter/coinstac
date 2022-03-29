c5a7d3f5c470dab9a0c837ecafa89d3713e2cbb2
kalitopix <kalitopix@gmail.com>
2022-03-29 06:00:38 -0400

    Fixed tutorial modal showing issue

--- packages/coinstac-ui/app/render/components/pipelines/pipeline.jsx
+++ packages/coinstac-ui/app/render/components/pipelines/pipeline.jsx
@@ -992,7 +992,7 @@ class Pipeline extends Component {
             </Typography>
           )}
         </ValidatorForm>
-        {!auth.hideTutorial && (
+        {!auth.isTutorialHidden && (
           <Joyride
             steps={STEPS.pipeline}
             continuous
