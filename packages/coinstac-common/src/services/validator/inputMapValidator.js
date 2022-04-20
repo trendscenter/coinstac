c931754a1efc755b02b2a77c5dbf44fddef72407
Dylan Martin <dylanrossmartin@gmail.com>
2022-04-20 14:40:05 -0400

    add overall validator

--- packages/coinstac-common/src/services/validator/inputMapValidator.js
+++ packages/coinstac-common/src/services/validator/inputMapValidator.js
@@ -74,8 +74,24 @@ function validator(opts, value) {
   }
 }
 
+const inputMapSchema = () => {
+  const schema = Joi.object({
+    data: Joi.object({
+      value: Joi.array().min(1).required(),
+    }).required(),
+    covariates: Joi.object({
+      value: Joi.array().min(1).required(),
+    }).required(),
+  }).unknown(true);
+  return schema;
+};
 
 function inputMapValidator(compSpecInput, inputMap, ownerOnly = false) {
+  const result = inputMapSchema().validate(inputMap);
+  if (result.error) {
+    throw result.error.message;
+  }
+
   Object.keys(inputMap).forEach((key) => {
     if (!ownerOnly || compSpecInput[key].source === 'owner') {
       try {
