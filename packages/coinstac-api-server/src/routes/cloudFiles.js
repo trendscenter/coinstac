0815de5cc529cb60fd232211940156becdcb345a
Dylan Martin <dylanrossmartin@gmail.com>
2022-06-07 15:11:22 -0400

    upload to s3

--- packages/coinstac-api-server/src/routes/cloudFiles.js
+++ packages/coinstac-api-server/src/routes/cloudFiles.js
@@ -1,4 +1,13 @@
 const helperFunctions = require('../auth-helpers');
+const AWS = require('aws-sdk');
+
+
+function uploadToS3(fileName, fileStream) {
+  const params = { Bucket: 'bucket', Key: fileName, Body: fileStream };
+  const s3 = new AWS.S3();
+  return s3.upload(params).promise();
+}
+
 
 module.exports = [
   {
@@ -9,9 +18,18 @@ module.exports = [
       pre: [
         // { method: helperFunctions.validateUser, assign: 'user' },
       ],
-      handler: (req, h) => {
+      handler: async (req, h) => {
         const { payload } = req;
-  
+        await Promise.all(
+          payload.file.map((fileStream) => {
+            const fileName = fileStream.hapi.filename;
+            fileStream.on('data', (d) => {
+              console.log(d);
+            });
+            return uploadToS3(fileName, fileStream);
+          })
+        );
+
         return h.response().code(201);
       },
       payload: {
