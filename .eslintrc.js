f8d3e5eb65eedd9e47fb127f858d4053f48e598d
Ross Kelly <5331533+rssk@users.noreply.github.com>
2022-06-09 20:44:55 -0600

    .

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,7 +27,6 @@ module.exports = {
     strict: [0, 'global'], // required for node, configurable for browser, https://github.com/eslint/eslint/issues/2785#issuecomment-113254153
     'arrow-body-style': 0,
     'consistent-return': 0,
-    'no-console': 0,
     'no-param-reassign': 0,
     'no-shadow': 0,
     'no-underscore-dangle': 0,
