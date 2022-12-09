fc280a5e108cc4829507f131334ff45b10a63426
Dylan Martin <dylanrossmartin@gmail.com>
2022-12-09 13:09:03 -0500

    gh actions deploy docs

--- coinstac-docs/docusaurus.config.js
+++ coinstac-docs/docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Coinstac',
   tagline: 'Decentralized Analysis Made Easy',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://trenscenter.github.io',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -16,8 +16,9 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'TReNDS', // Usually your GitHub org/user name.
-  projectName: 'Coinstac', // Usually your repo name.
+  organizationName: 'trendscenter', // Usually your GitHub org/user name.
+  projectName: 'coinstac', // Usually your repo name.
+  trailingSlash: 'false',
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
