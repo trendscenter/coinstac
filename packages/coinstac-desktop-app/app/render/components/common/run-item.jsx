fb11543e5022a636a18581221e49a92df3d40dc2
Ross Kelly <5331533+rssk@users.noreply.github.com>
2024-06-06 13:40:36 -0400

    merge master

--- packages/coinstac-desktop-app/app/render/components/common/run-item.jsx
+++ packages/coinstac-desktop-app/app/render/components/common/run-item.jsx
@@ -7,7 +7,6 @@ import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import LinearProgress from '@material-ui/core/LinearProgress';
-import Paper from '@material-ui/core/Paper';
 import makeStyles from '@material-ui/core/styles/makeStyles';
 import Typography from '@material-ui/core/Typography';
 import classNames from 'classnames';
