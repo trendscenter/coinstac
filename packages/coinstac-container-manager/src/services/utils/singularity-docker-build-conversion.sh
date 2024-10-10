525ae7a67222987944ef908d884d33c9e13e11a0
Ross Kelly <5331533+rssk@users.noreply.github.com>
2024-10-10 16:08:34 -0400

    hotfix singularity build issue

--- packages/coinstac-container-manager/src/services/utils/singularity-docker-build-conversion.sh
+++ packages/coinstac-container-manager/src/services/utils/singularity-docker-build-conversion.sh
@@ -1,30 +1,42 @@
 #!/usr/bin/env bash
 IMAGE_PATH=$1
+IMAGE_NAME=$(basename $IMAGE_PATH)
 DOCKER_IMAGE_NAME=$2
-WORK_PATH="/tmp/$IMAGE_PATH"
+WORK_PATH="/tmp/coinstacImages"
+#${IMAGE_PATH#/}"
 
-
-singularity build -F --fix-perms --sandbox $WORK_PATH docker://$DOCKER_IMAGE_NAME
+mkdir -p $WORK_PATH
 code=$?
 if [ $code != 0 ]; then
-  >&2 echo "After build 1"
+  >&2 echo "After making dir "
   >&2 echo "WORK_PATH = ${WORK_PATH}"
   >&2 echo "DOCKER_IMAGE_NAME = ${DOCKER_IMAGE_NAME}"
   exit $code
 fi
 
-cp --preserve=links "$WORK_PATH/.singularity.d/runscript" "$WORK_PATH/.singularity.d/startscript"
+singularity build -F --fix-perms --sandbox "$WORK_PATH/$IMAGE_NAME" docker://$DOCKER_IMAGE_NAME
 code=$?
 if [ $code != 0 ]; then
-  >&2 echo "After cp"
-  >&2 echo "WORK_PATH = ${$WORK_PATH}"
+  >&2 echo "After build 1"
+  >&2 echo "WORK_PATH = ${WORK_PATH}"
+  >&2 echo "WORK_PATH w image = $WORK_PATH/$IMAGE_NAME"
   >&2 echo "DOCKER_IMAGE_NAME = ${DOCKER_IMAGE_NAME}"
   exit $code
 fi
 
-singularity build -F --fix-perms $WORK_PATH.sif $WORK_PATH
+cp --preserve=links "$WORK_PATH/$IMAGE_NAME/.singularity.d/runscript" "$WORK_PATH/$IMAGE_NAME/.singularity.d/startscript"
+code=$?
+if [ $code != 0 ]; then
+  >&2 echo "After cp"
+  >&2 echo "WORK_PATH = ${$WORK_PATH}"
+  >&2 echo "WORK_PATH w image = $WORK_PATH/$IMAGE_NAME"
+  >&2 echo "DOCKER_IMAGE_NAME = ${DOCKER_IMAGE_NAME}"
+  exit $code
+fi
 
-mv $WORK_PATH.sif $IMAGE_PATH.sif
+singularity build -F --fix-perms "$WORK_PATH/$IMAGE_NAME.sif" "$WORK_PATH/$IMAGE_NAME"
+
+mv $WORK_PATH/$IMAGE_NAME.sif $IMAGE_PATH.sif
 
 rm -rf $WORK_PATH
 
